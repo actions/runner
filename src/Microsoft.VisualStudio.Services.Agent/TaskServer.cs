60c4b4ae135629f18760a01811818eed3a0b7667
Eric Sciple <ersciple@microsoft.com>
2016-02-03 23:55:37 -0500

    Fix async method names.

--- src/Microsoft.VisualStudio.Services.Agent/TaskServer.cs
+++ src/Microsoft.VisualStudio.Services.Agent/TaskServer.cs
@@ -5,31 +5,31 @@ namespace Microsoft.VisualStudio.Services.Agent
 {
     public interface ITaskServer
     {
-        Task<String> CreateAgentSession(Int32 poolId);
-        Task DeleteAgentMessage(Int32 poolId, String sessionId, AgentMessage message);
-        Task DeleteAgentSession(Int32 poolId, String sessionId);
-        Task<AgentMessage> GetAgentMessage(Int32 poolId, String sessionId, Int32 lastMessageId);
+        Task<String> CreateAgentSessionAsync(Int32 poolId);
+        Task DeleteAgentMessageAsync(Int32 poolId, String sessionId, AgentMessage message);
+        Task DeleteAgentSessionAsync(Int32 poolId, String sessionId);
+        Task<AgentMessage> GetAgentMessageAsync(Int32 poolId, String sessionId, Int32 lastMessageId);
     }
 
     public sealed class TaskServer : ITaskServer
     {
-        public Task<String> CreateAgentSession(Int32 poolId)
+        public Task<String> CreateAgentSessionAsync(Int32 poolId)
         {
             // TODO: Pass through to the REST SDK.
             throw new System.NotImplementedException();
         }
 
-        public Task DeleteAgentMessage(Int32 poolId, String sessionId, AgentMessage message)
+        public Task DeleteAgentMessageAsync(Int32 poolId, String sessionId, AgentMessage message)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task DeleteAgentSession(Int32 poolId, String sessionId)
+        public Task DeleteAgentSessionAsync(Int32 poolId, String sessionId)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<AgentMessage> GetAgentMessage(Int32 poolId, String sessionId, Int32 lastMessageId)
+        public Task<AgentMessage> GetAgentMessageAsync(Int32 poolId, String sessionId, Int32 lastMessageId)
         {
             throw new System.NotImplementedException();
         }
