82b5493cd2a86a8c9f8b193eb69d86384ef84bc6
Stan Iliev <stiliev@microsoft.com>
2016-03-10 18:08:06 -0500

    add timeouts in IPC communication between agent.listener and agent.worker process

--- src/Agent.Listener/JobDispatcher.cs
+++ src/Agent.Listener/JobDispatcher.cs
@@ -16,10 +16,12 @@ namespace Microsoft.VisualStudio.Services.Agent.Listener
     public sealed class JobDispatcher : AgentService, IJobDispatcher
     {
 #if OS_WINDOWS
-        private const String WorkerProcessName = "Agent.Worker.exe";
+        private const string WorkerProcessName = "Agent.Worker.exe";
 #else
-        private const String WorkerProcessName = "Agent.Worker";
-#endif        
+        private const string WorkerProcessName = "Agent.Worker";
+#endif
+        //allow up to 30sec for any data to be transmitted over the process channel
+        private readonly TimeSpan ChannelTimeout = TimeSpan.FromSeconds(30);
 
         public async Task<int> RunAsync(JobRequestMessage message, CancellationToken token)
         {
@@ -35,7 +37,11 @@ namespace Microsoft.VisualStudio.Services.Agent.Listener
                         workerProcessTask = processInvoker.ExecuteAsync(assemblyDirectory, workerFileName, "spawnclient " + pipeHandleOut + " " + pipeHandleIn, null, token);
                     }
                 );
-                await processChannel.SendAsync(MessageType.NewJobRequest, JsonUtility.ToString(message), token);
+
+                //send the job request
+                var ct1 = new CancellationTokenSource(ChannelTimeout);
+                await processChannel.SendAsync(MessageType.NewJobRequest, JsonUtility.ToString(message), ct1.Token);
+
                 int resultCode = 0;
                 bool canceled = false;
                 try
@@ -54,10 +60,9 @@ namespace Microsoft.VisualStudio.Services.Agent.Listener
                 }
                 if (canceled)
                 {
-                    //we create internal cancelation token, which nobody can access, because the parent token is already cancelled
-                    //Hopefully not an issue, because cancelation should not take long
-                    CancellationTokenSource ct = new CancellationTokenSource();
-                    await processChannel.SendAsync(MessageType.CancelRequest, "", ct.Token);
+                    //we create internal cancellation token, because the parent token is already cancelled
+                    CancellationTokenSource ct2 = new CancellationTokenSource(ChannelTimeout);
+                    await processChannel.SendAsync(MessageType.CancelRequest, string.Empty, ct2.Token);
                 }
                 return resultCode;
             }
