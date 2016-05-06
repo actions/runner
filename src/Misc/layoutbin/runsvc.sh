d7cf0540ef82bde0f00ba68762466ca01bbaec25
Stan Iliev <stiliev@microsoft.com>
2016-05-06 15:09:10 -0400

    fix for service stop on OS X

--- src/Misc/layoutbin/runsvc.sh
+++ src/Misc/layoutbin/runsvc.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# convert SIGTERM signal to SIGINT
+trap 'kill -INT $PID' TERM INT
+
 if [ -f ".Path" ]; then
     # configure
     export PATH=`cat .Path`
@@ -9,4 +12,8 @@ fi
 # insert anything to setup env when running as a service
 
 # run the host process which keep the listener alive
-./externals/node/bin/node ./bin/AgentService.js
+./externals/node/bin/node ./bin/AgentService.js &
+PID=$!
+wait $PID
+trap - TERM INT
+wait $PID
