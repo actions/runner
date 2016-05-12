39dfc89125cd44c99753d36fe7a2e7b89a0053df
Bryan MacFarlane <bryanmac@microsoft.com>
2016-05-12 15:44:35 -0400

    run checks if configured first

--- src/Misc/layoutroot/run.sh
+++ src/Misc/layoutroot/run.sh
@@ -1,1 +1,13 @@
+user_id=`id -u`
+
+if [ $user_id -eq 0 ]; then
+    echo "Must not run interactively with sudo"
+    exit 1
+fi
+
+if [ ! -f .Agent ]; then
+    echo "Must configure first. Run ./config.sh"
+    exit 1
+fi
+
 ./bin/Agent.Listener $*
