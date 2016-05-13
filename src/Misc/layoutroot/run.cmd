b9cb28d7921b769eb910499b859e35736e2e013a
Eric Sciple <ersciple@microsoft.com>
2016-05-13 11:39:05 -0400

    Unblock files.

--- src/Misc/layoutroot/run.cmd
+++ src/Misc/layoutroot/run.cmd
@@ -1,8 +1,12 @@
-@setlocal
 @echo off
+setlocal
+if defined VERBOSE_ARG (
+  set VERBOSE_ARG='Continue'
+) else (
+  set VERBOSE_ARG='SilentlyContinue'
+)
 
-REM TODO: unblock files
+powershell.exe -NoLogo -Sta -NoProfile -NonInteractive -ExecutionPolicy Unrestricted -Command "$VerbosePreference = %VERBOSE_ARG% ; Get-ChildItem -LiteralPath '%~dp0' | ForEach-Object { Write-Verbose ('Unblock: {0}' -f $_.FullName) ; $_ } | Unblock-File | Out-Null ; Get-ChildItem -Recurse -LiteralPath '%~dp0bin', '%~dp0externals' | Where-Object { $_ -match '\.(ps1|psd1|psm1)$' } | ForEach-Object { Write-Verbose ('Unblock: {0}' -f $_.FullName) ; $_ } | Unblock-File | Out-Null"
 
 :run
 "%~dp0bin\Agent.Listener.exe" %*
-
