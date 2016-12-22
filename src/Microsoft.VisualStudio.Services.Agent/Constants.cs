4966268c15c5bb4fdb84cf5fb78f59aa917edd5c
Eric Sciple <ersciple@microsoft.com>
2016-12-22 11:13:33 -0500

    2.111.0

--- src/Microsoft.VisualStudio.Services.Agent/Constants.cs
+++ src/Microsoft.VisualStudio.Services.Agent/Constants.cs
@@ -39,7 +39,7 @@ namespace Microsoft.VisualStudio.Services.Agent
 
         public static class Agent
         {
-            public static readonly string Version = "2.110.0";
+            public static readonly string Version = "2.111.0";
 
 #if OS_LINUX
             public static readonly OSPlatform Platform = OSPlatform.Linux;
