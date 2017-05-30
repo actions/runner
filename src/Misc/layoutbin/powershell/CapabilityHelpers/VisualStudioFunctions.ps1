25914248fc07e95594290057e9060e086f0cd78d
ericsciple <ericsciple@users.noreply.github.com>
2017-05-30 10:41:23 -0400

    VS build tools (#994)

--- src/Misc/layoutbin/powershell/CapabilityHelpers/VisualStudioFunctions.ps1
+++ src/Misc/layoutbin/powershell/CapabilityHelpers/VisualStudioFunctions.ps1
@@ -25,8 +25,30 @@ function Get-VisualStudio_15_0 {
             }
         Write-Host "Exit code: $LASTEXITCODE"
         if ($LASTEXITCODE -eq 0) {
-            return (ConvertFrom-Json -InputObject $output.ToString()) |
+            $instance = (ConvertFrom-Json -InputObject $output.ToString()) |
                 Select-Object -First 1
+            if (!$instance) {
+                Write-Host "Getting latest BuildTools 15 setup instance."
+                $output = New-Object System.Text.StringBuilder
+                Write-Host "& $PSScriptRoot\..\..\..\externals\vswhere\vswhere.exe -version [15.0,16.0) -products Microsoft.VisualStudio.Product.BuildTools -latest -format json"
+                & $PSScriptRoot\..\..\..\externals\vswhere\vswhere.exe -version '[15.0,16.0)' -products Microsoft.VisualStudio.Product.BuildTools -latest -format json 2>&1 |
+                    ForEach-Object {
+                        if ($_ -is [System.Management.Automation.ErrorRecord]) {
+                            Write-Host "STDERR: $($_.Exception.Message)"
+                        }
+                        else {
+                            Write-Host $_
+                            $null = $output.AppendLine($_)
+                        }
+                    }
+                Write-Host "Exit code: $LASTEXITCODE"
+                if ($LASTEXITCODE -eq 0) {
+                    $instance = (ConvertFrom-Json -InputObject $output.ToString()) |
+                        Select-Object -First 1
+                }
+            }
+
+            return $instance
         }
     } catch {
         Write-Host ($_ | Out-String)
