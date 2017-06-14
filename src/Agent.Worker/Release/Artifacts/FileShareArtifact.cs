e4611e9da8b3ce7fbb20f6e72e15f121fec319f0
SunnyKumarR <sunkum@microsoft.com>
2017-06-14 10:54:34 +0530

    Fixing issues in Robocopy based download in RM
    Fixing issues in Robocopy based download in RM

--- src/Agent.Worker/Release/Artifacts/FileShareArtifact.cs
+++ src/Agent.Worker/Release/Artifacts/FileShareArtifact.cs
@@ -120,7 +120,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.Release.Artifacts
 
                 dropLocation = Path.Combine(dropLocation.TrimEnd(trimChars), relativePath.Trim(trimChars));
 
-                string robocopyArguments = dropLocation + " " + downloadFolderPath + " /E /Z /NP";
+                string robocopyArguments = "\"" + dropLocation + "\" \"" + downloadFolderPath + "\" /E /Z /NP /R:3";
                 if (verbose != true)
                 {
                     robocopyArguments = robocopyArguments + " /NDL /NFL";
