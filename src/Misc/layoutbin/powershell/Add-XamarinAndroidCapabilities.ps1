2b1240da20be85aa10660e144e0a43169710cd6e
Yang Cao <yacao@microsoft.com>
2017-08-23 10:22:22 -0400

    Update Xamarin capability provider to work with VS 2017 (#1135)
    * Update Xamarin capability provider to work with VS 2017
    
    * Only take first line and trim the result
    
    * Change casing on the powershell script

--- src/Misc/layoutbin/powershell/Add-XamarinAndroidCapabilities.ps1
+++ src/Misc/layoutbin/powershell/Add-XamarinAndroidCapabilities.ps1
@@ -1,4 +1,19 @@
 [CmdletBinding()]
 param()
 
-$null = Add-CapabilityFromRegistry -Name 'Xamarin.Android' -Hive 'LocalMachine' -View 'Registry32' -KeyName 'Software\Novell\Mono for Android' -ValueName 'InstalledVersion'
+if (!(Add-CapabilityFromRegistry -Name 'Xamarin.Android' -Hive 'LocalMachine' -View 'Registry32' -KeyName 'Software\Novell\Mono for Android' -ValueName 'InstalledVersion')) {
+    $vs15 = Get-VisualStudio_15_0
+    if ($vs15 -and $vs15.installationPath) {
+        # End with "\" for consistency with old ShellFolder values.
+        $shellFolder15 = $vs15.installationPath.TrimEnd('\'[0]) + "\"
+        $xamarinAndroidDir = ([System.IO.Path]::Combine($shellFolder15, 'MSBuild', 'Xamarin', 'Android')) + '\'
+        if ((Test-Container -LiteralPath $xamarinAndroidDir)) {
+            $versionFile = ([System.IO.Path]::Combine($xamarinAndroidDir, 'Version'))
+            $version = Get-Content -ErrorAction ignore -TotalCount 1 -LiteralPath $versionFile 
+            if ($version) {
+                Write-Capability -Name 'Xamarin.Android' -Value $version.trim()
+            }
+        }
+    }
+}
+
