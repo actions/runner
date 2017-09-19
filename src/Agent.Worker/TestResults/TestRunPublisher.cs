3276f2c496f540f1ffe37669e6f80c14db77694d
Parag Gangil <pagangil@microsoft.com>
2017-09-19 16:03:57 +0530

    Changing console error attachment name as per requirement

--- src/Agent.Worker/TestResults/TestRunPublisher.cs
+++ src/Agent.Worker/TestResults/TestRunPublisher.cs
@@ -329,7 +329,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.TestResults
             Trace.Entering();
             if (string.IsNullOrWhiteSpace(stdErr) == false)
             {
-                const string stdErrFileName = "Standard_Error_Output.log";
+                const string stdErrFileName = "Standard_Console_Error.log";
 
                 if (stdErr.Length <= TCM_MAX_FILESIZE)
                 {
