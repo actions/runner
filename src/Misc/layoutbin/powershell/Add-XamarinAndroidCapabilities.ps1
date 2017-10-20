c1cd9fcf43f2971abfe8cb760e0c232935b3ed88
Yang Cao <yacao@microsoft.com>
2017-10-20 11:18:39 -0400

    Xamarin 8 changed version file to version.txt (#1254)

--- src/Misc/layoutbin/powershell/Add-XamarinAndroidCapabilities.ps1
+++ src/Misc/layoutbin/powershell/Add-XamarinAndroidCapabilities.ps1
@@ -8,10 +8,14 @@ if (!(Add-CapabilityFromRegistry -Name 'Xamarin.Android' -Hive 'LocalMachine' -V
         $shellFolder15 = $vs15.installationPath.TrimEnd('\'[0]) + "\"
         $xamarinAndroidDir = ([System.IO.Path]::Combine($shellFolder15, 'MSBuild', 'Xamarin', 'Android')) + '\'
         if ((Test-Container -LiteralPath $xamarinAndroidDir)) {
-            $versionFile = ([System.IO.Path]::Combine($xamarinAndroidDir, 'Version'))
-            $version = Get-Content -ErrorAction ignore -TotalCount 1 -LiteralPath $versionFile 
-            if ($version) {
-                Write-Capability -Name 'Xamarin.Android' -Value $version.trim()
+            # Xamarin.Android 7 has a Version file, and this file is renamed to Version.txt in Xamarin.Android 8.x
+            foreach ($file in @('Version', 'Version.txt')) {
+                $versionFile = ([System.IO.Path]::Combine($xamarinAndroidDir, $file))
+                $version = Get-Content -ErrorAction ignore -TotalCount 1 -LiteralPath $versionFile
+                if ($version) {
+                    Write-Capability -Name 'Xamarin.Android' -Value $version.trim()
+                    break
+                }
             }
         }
     }
