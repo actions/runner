5539ec4485c2d286f198ce275e7f62acd08c3ff6
Ting <tihuang@microsoft.com>
2017-12-21 11:43:29 -0500

    bump agent version to 2.127.0

--- src/Microsoft.VisualStudio.Services.Agent/Constants.cs
+++ src/Microsoft.VisualStudio.Services.Agent/Constants.cs
@@ -45,7 +45,7 @@ namespace Microsoft.VisualStudio.Services.Agent
 
         public static class Agent
         {
-            public static readonly string Version = "2.126.0";
+            public static readonly string Version = "2.127.0";
 
 #if OS_LINUX
             public static readonly OSPlatform Platform = OSPlatform.Linux;
