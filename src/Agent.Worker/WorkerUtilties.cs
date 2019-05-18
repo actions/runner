ab215896c667c247fcb0f9f3f2f1f9c7a242436c
Eric Sciple <ersciple@microsoft.com>
2019-05-18 01:23:16 -0400

    Switch to context data, 0.5.117-private, comment broken client OM references

--- src/Agent.Worker/WorkerUtilties.cs
+++ src/Agent.Worker/WorkerUtilties.cs
@@ -89,7 +89,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
                 variables: scrubbedVariables,
                 maskHints: message.MaskHints,
                 jobResources: scrubbedJobResources,
-                expressionValues: message.ExpressionValues,
+                contextData: message.ContextData,
                 workspaceOptions: message.Workspace,
                 steps: message.Steps);
         }
