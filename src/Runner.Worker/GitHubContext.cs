1f3fd2efb0750620557618e3188933b97f70eedd
Marianna Sternefeld <marianna@sternefeld.com>
2019-06-05 13:42:11 -0400

    Moved classes to their own Files and Added Fix for GitHub Context (#16)
    * Added changes to fix Issue
    
    * Fixed minor issue

---
+++ src/Runner.Worker/GitHubContext.cs
@@ -0,0 +1,15 @@
+using GitHub.DistributedTask.Pipelines.ContextData;
+using System;
+using System.Collections.Generic;
+
+public sealed class GitHubContext : DictionaryContextData, IEnvironmentContextData
+    {
+        public IEnumerable<KeyValuePair<string, string>> GetRuntimeEnvironmentVariables()
+        {
+            foreach (var data in this)
+            {
+                if(!data.Key.Equals("token"))
+                    yield return new KeyValuePair<string, string>($"GITHUB_{data.Key.ToUpperInvariant()}", data.Value as StringContextData);
+            }
+        }
+    }
