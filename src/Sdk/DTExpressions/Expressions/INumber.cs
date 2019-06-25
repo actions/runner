565fe7f3915913d38fab274dc94eb5079b331f4b
Marianna Sternefeld <t-maste@microsoft.com>
2019-06-25 12:30:24 -0400

    Added most of the other repo's files/code (#34)

---
+++ src/Sdk/DTExpressions/Expressions/INumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace GitHub.DistributedTask.Expressions
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public interface INumber
+    {
+        Double GetDouble();
+    }
+}
---
+++ src/Sdk/DTPipelines/Pipelines/ContextData/BooleanContextData.cs
@@ -0,0 +1,58 @@
+﻿using GitHub.DistributedTask.Expressions;
+using GitHub.Services.WebApi.Internal;
+using Newtonsoft.Json.Linq;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace GitHub.DistributedTask.Pipelines.ContextData
+{
+    [DataContract]
+    [ClientIgnore]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public sealed class BooleanContextData : PipelineContextData, IBoolean
+    {
+        public BooleanContextData(Boolean value)
+            : base(PipelineContextDataType.Boolean)
+        {
+            m_value = value;
+        }
+
+        public Boolean Value
+        {
+            get
+            {
+                return m_value;
+            }
+        }
+
+        public override PipelineContextData Clone()
+        {
+            return new BooleanContextData(m_value);
+        }
+
+        public override JToken ToJToken()
+        {
+            return (JToken)m_value;
+        }
+
+        public bool GetBoolean()
+        {
+            return Value;
+        }
+
+        public static implicit operator Boolean(BooleanContextData data)
+        {
+            return data.Value;
+        }
+
+        public static implicit operator BooleanContextData(Boolean data)
+        {
+            return new BooleanContextData(data);
+        }
+
+        [DataMember(Name = "b", EmitDefaultValue = false)]
+        private Boolean m_value;
+    }
+}
