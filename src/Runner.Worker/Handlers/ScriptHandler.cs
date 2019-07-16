ce69f12ab60c25d340b6be99168d44b2d15fbe40
David Kale <dakale@microsoft.com>
2019-07-15 21:11:42 -0400

    run-action shell option (#43)
    * Grab 'shell' from script inputs
    
    * Use given shell option
    
    * Use default arguments if no shell given
    
    * Add file extension on windows
    
    * WIP: 1st class support for cmd/powershell, allow {shell: command} {script} or template
    
    * Remove -eo pipefail for bash default
    
    * Write file relative to worker proc host not stephost
    
    * --no-profile etc are not valid POSIX sh options
    
    * Set e for bash/sh
    
    * Pipefail aint posix
    
    * Undefault -e
    
    * Require shell to be builtin or format string
    
    * concisify
    
    * PR feedback and add python builtin
    
    * Fix

--- src/Runner.Worker/Handlers/ScriptHandler.cs
+++ src/Runner.Worker/Handlers/ScriptHandler.cs
@@ -39,41 +39,63 @@ namespace GitHub.Runner.Worker.Handlers
             var workspaceDir = githubContext["workspace"] as StringContextData;
             workingDirectory = Path.Combine(workspaceDir, workingDirectory ?? string.Empty);
 
-#if OS_WINDOWS
-            // Fixup contents
-            contents = contents.Replace("\r\n", "\n").Replace("\n", "\r\n");
-            // Note, use @echo off instead of using the /Q command line switch.
-            // When /Q is used, echo can't be turned on.
-            contents = $"@echo off\r\n{contents}";
+            Inputs.TryGetValue("shell", out var shell);
+            var isContainerStepHost = StepHost is ContainerStepHost;
 
-            // Write the script
-            var filePath = Path.Combine(tempDirectory, $"{Guid.NewGuid()}.cmd");
+            string commandPath, argFormat, shellCommand;
+            // Set up default command and arguments
+            if (string.IsNullOrEmpty(shell))
+            {
+#if OS_WINDOWS
+                shellCommand = "cmd";
+                commandPath = System.Environment.GetEnvironmentVariable("ComSpec");
+                ArgUtil.NotNullOrEmpty(commandPath, "%ComSpec%");
+#else
+                shellCommand = "sh";
+                commandPath = WhichUtil.Which("bash") ?? WhichUtil.Which("sh", true);
+#endif
+                argFormat = ScriptHandlerHelpers.GetScriptArgumentsFormat(shellCommand);
+            }
+            else
+            {
+                var parsed = ScriptHandlerHelpers.ParseShellOptionString(shell);
+                shellCommand = parsed.shellCommand;
+                // For non-ContainerStepHost, the command must be located on the host by Which
+                commandPath = WhichUtil.Which(parsed.shellCommand, !isContainerStepHost);
+                argFormat = $"{parsed.shellArgs}".TrimStart();
+                if (string.IsNullOrEmpty(argFormat))
+                {
+                    argFormat = ScriptHandlerHelpers.GetScriptArgumentsFormat(shellCommand);
+                }
+            }
+
+            // No arg format was given, shell must be a built-in
+            if (string.IsNullOrEmpty(argFormat) || !argFormat.Contains("{0}"))
+            {
+                throw new ArgumentException("Invalid shell option. Shell must be a valid built-in (bash, sh, cmd, powershell, pwsh) or a format string containing '{0}'");
+            }
+
+            // We do not not the full path until we know what shell is being used, so that we can determine the file extension
+            var scriptFilePath = Path.Combine(tempDirectory, $"{Guid.NewGuid()}{ScriptHandlerHelpers.GetScriptFileExtension(shellCommand)}");
+            var resolvedScriptPath = $"{StepHost.ResolvePathForStepHost(scriptFilePath).Replace("\"", "\\\"")}";
+
+            // Format arg string with script path
+            var arguments = string.Format(argFormat, resolvedScriptPath);
+
+            // Fix up and write the script
+            contents = ScriptHandlerHelpers.FixUpScriptContents(shellCommand, contents);
+#if OS_WINDOWS
+            // Normalize Windows line endings
+            contents = contents.Replace("\r\n", "\n").Replace("\n", "\r\n");
             var encoding = ExecutionContext.Variables.Retain_Default_Encoding && Console.InputEncoding.CodePage != 65001
                 ? Console.InputEncoding
                 : new UTF8Encoding(false);
-            File.WriteAllText(filePath, contents, encoding);
-
-            // Command path
-            var commandPath = System.Environment.GetEnvironmentVariable("ComSpec");
-            ArgUtil.NotNullOrEmpty(commandPath, "%ComSpec%");
-
-            // Arguments
-            var arguments = $"/D /E:ON /V:OFF /S /C \"CALL \"{StepHost.ResolvePathForStepHost(filePath)}\"\"";
 #else
-            // Fixup contents
-            contents = $"set -eo pipefail\n{contents}";
-
-            // Write the script
-            var filePath = Path.Combine(tempDirectory, $"{Guid.NewGuid()}.sh");
             // Don't add a BOM. It causes the script to fail on some operating systems (e.g. on Ubuntu 14).
-            File.WriteAllText(filePath, contents, new UTF8Encoding(false));
-
-            // Command path
-            var commandPath = WhichUtil.Which("bash") ?? WhichUtil.Which("sh", true);
-
-            // Arguments
-            var arguments = $"--noprofile --norc {StepHost.ResolvePathForStepHost(filePath).Replace("\"", "\\\"")}";
+            var encoding = new UTF8Encoding(false);
 #endif
+            // Script is written to local path (ie host) but executed relative to the StepHost, which may be a container
+            File.WriteAllText(scriptFilePath, contents, encoding);
 
             ExecutionContext.Output("Script contents:");
             ExecutionContext.Output(contents);
@@ -95,7 +117,7 @@ namespace GitHub.Runner.Worker.Handlers
             }
 
             // dump out the command
-            var fileName = StepHost.ResolvePathForStepHost(commandPath);
+            var fileName = isContainerStepHost ? shellCommand : commandPath;
             ExecutionContext.Command($"{fileName} {arguments}");
 
             using (var stdoutManager = new OutputManager(ExecutionContext, ActionCommandManager))
