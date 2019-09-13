77e8bf79ebfe4b2f5e0dea2bbd9529a1562d2101
Julio Barba <juliobv@microsoft.com>
2019-09-13 13:51:16 -0400

    Allow vswhere to find VS pre-releases

--- src/dev.sh
+++ src/dev.sh
@@ -218,7 +218,7 @@ bash ./Misc/externals.sh $RUNTIME_ID "Pre-Cache" || checkRC "externals.sh Pre-Ca
 
 if [[ "$CURRENT_PLATFORM" == 'windows' ]]; then
     vswhere=$(find "$DOWNLOAD_DIR" -name vswhere.exe | head -1)
-    vs_location=$("$vswhere" -latest -property installationPath)
+    vs_location=$("$vswhere" -prerelease -latest -property installationPath)
     msbuild_location="$vs_location""\MSBuild\15.0\Bin\msbuild.exe"
 
     if [[ ! -e "${msbuild_location}" ]]; then
