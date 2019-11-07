7d28a57680c20c96c7b492a2af648f32e58938a3
TingluoHuang <TingluoHuang@github.com>
2019-11-07 15:52:28 -0500

    c

--- src/Runner.Common/Util/VarUtil.cs
+++ src/Runner.Common/Util/VarUtil.cs
@@ -54,6 +54,8 @@ namespace GitHub.Runner.Common.Util
                         return "X64";
                     case Constants.Architecture.Arm:
                         return "ARM";
+                    case Constants.Architecture.Arm64:
+                        return "ARM64";    
                     default:
                         throw new NotSupportedException(); // Should never reach here.
                 }
