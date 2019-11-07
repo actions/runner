f1b36bb9574e38e3644c70653813ad9cdd6a041e
David Kale <dakale@microsoft.com>
2019-11-07 14:28:02 -0500

    Fix formatting. Remove piplines

--- src/dev.sh
+++ src/dev.sh
@@ -34,29 +34,28 @@ if [[ ($(uname) == "Linux") || ($(uname) == "Darwin") ]]; then
 fi
 
 if [[ "$CURRENT_PLATFORM" == 'windows' ]]; then
-RUNTIME_ID='win-x64'
-if [[ "$PROCESSOR_ARCHITECTURE" == 'x86' ]]; then
-    RUNTIME_ID='win-x86'
-fi
-elif [[ "$CURRENT_PLATFORM" == 'linux' ]]; then
-RUNTIME_ID="linux-x64"
-if command -v uname > /dev/null; then
-    CPU_NAME=$(uname -m)
-    case $CPU_NAME in
-        armv7l) RUNTIME_ID="linux-arm";;
-        aarch64) RUNTIME_ID="linux-arm64";;
-    esac
-fi
-
-if [ -e /etc/redhat-release ]; then
-    redhatRelease=$(</etc/redhat-release)
-    if [[ $redhatRelease == "CentOS release 6."* || $redhatRelease == "Red Hat Enterprise Linux Server release 6."* ]]; then
-        RUNTIME_ID='rhel.6-x64'
+    RUNTIME_ID='win-x64'
+    if [[ "$PROCESSOR_ARCHITECTURE" == 'x86' ]]; then
+        RUNTIME_ID='win-x86'
     fi
-fi
+elif [[ "$CURRENT_PLATFORM" == 'linux' ]]; then
+    RUNTIME_ID="linux-x64"
+    if command -v uname > /dev/null; then
+        CPU_NAME=$(uname -m)
+        case $CPU_NAME in
+            armv7l) RUNTIME_ID="linux-arm";;
+            aarch64) RUNTIME_ID="linux-arm64";;
+        esac
+    fi
 
+    if [ -e /etc/redhat-release ]; then
+        redhatRelease=$(</etc/redhat-release)
+        if [[ $redhatRelease == "CentOS release 6."* || $redhatRelease == "Red Hat Enterprise Linux Server release 6."* ]]; then
+            RUNTIME_ID='rhel.6-x64'
+        fi
+    fi
 elif [[ "$CURRENT_PLATFORM" == 'darwin' ]]; then
-RUNTIME_ID='osx-x64'
+    RUNTIME_ID='osx-x64'
 fi
 
 if [[ -n "$DEV_TARGET_RUNTIME" ]]; then
