f78d35dc4e54f3398bed18c2cdaad6ba7e53238d
Julio Barba <juliobbv@github.com>
2019-12-12 13:53:12 -0500

    Trim Build2 SDK (#219)
    * Trim Build2 SDK REST API methods
    * Remove unused files

--- src/Sdk/BuildWebApi/Api/ArtifactResourceTypes.cs
+++ src/Sdk/BuildWebApi/Api/ArtifactResourceTypes.cs
@@ -1,76 +1,14 @@
 ﻿using System;
-using System.ComponentModel;
 using GitHub.Services.Common;
 
 namespace GitHub.Build.WebApi
 {
-    [Obsolete("Use ArtifactResourceTypes instead.")]
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public static class WellKnownArtifactResourceTypes
-    {
-        public const String FilePath = ArtifactResourceTypes.FilePath;
-        public const String SymbolStore = ArtifactResourceTypes.SymbolStore;
-        public const String VersionControl = ArtifactResourceTypes.VersionControl;
-        public const String Container = ArtifactResourceTypes.Container;
-        public const String GitRef = ArtifactResourceTypes.GitRef;
-        public const String TfvcLabel = ArtifactResourceTypes.TfvcLabel;
-        public const String SymbolRequest = ArtifactResourceTypes.SymbolRequest;
-    }
-
-    [GenerateAllConstants]
     public static class ArtifactResourceTypes
     {
-        /// <summary>
-        /// UNC or local folder path
-        /// E.g. \\vscsstor\CIDrops\CloudU.Gated\140317.115955 or file://vscsstor/CIDrops/CloudU.Gated/140317.115955
-        /// </summary>
-        public const String FilePath = "FilePath";
-
-        /// <summary>
-        /// Symbol store UNC path
-        /// E.g. \\symbolstore
-        /// </summary>
-        public const String SymbolStore = "SymbolStore";
-
-        /// <summary>
-        /// TF VC server folder path
-        /// E.g. $/Dev1/Drops/CloudU.Gated/140317.115955
-        /// </summary>
-        public const String VersionControl = "VersionControl";
-
         /// <summary>
         /// Build container reference
         /// E.g. #/2121/drop
         /// </summary>
         public const String Container = "Container";
-
-        /// <summary>
-        /// Git ref
-        /// E.g. refs/tags/MyCIDefinition.Buildable
-        /// </summary>
-        public const String GitRef = "GitRef";
-
-        /// <summary>
-        /// TFVC label
-        /// </summary>
-        public const String TfvcLabel = "TfvcLabel";
-
-        /// <summary>
-        /// Symbol store URL 
-        /// E.g. https://mseng.artifacts.visualstudio.com/...
-        /// </summary>
-        public const String SymbolRequest = "SymbolRequest";
-
-        /// <summary>
-        /// Dedup Drop (old name fo PipelineArtifact)
-        /// E.g. drop1
-        /// </summary>
-        public const String Drop = "Drop";
-
-        /// <summary>
-        /// Dedup'ed pipeline artifact
-        /// E.g. artifact1
-        /// </summary>
-        public const String PipelineArtifact = "PipelineArtifact";
     }
 }
--- src/Sdk/BuildWebApi/Api/Contracts/ArtifactResource.cs
+++ src/Sdk/BuildWebApi/Api/Contracts/ArtifactResource.cs
@@ -50,46 +50,5 @@ namespace GitHub.Build.WebApi
             get;
             set;
         }
-
-        /// <summary>
-        /// The full http link to the resource.
-        /// </summary>
-        [DataMember(IsRequired = false, EmitDefaultValue = false)]
-        public String Url
-        {
-            get;
-            set;
-        }
-
-        /// <summary>
-        /// A link to download the resource.
-        /// </summary>
-        /// <remarks>
-        /// This might include things like query parameters to download as a zip file.
-        /// </remarks>
-        [DataMember(IsRequired = false, EmitDefaultValue = false)]
-        public String DownloadUrl
-        {
-            get;
-            set;
-        }
-
-        /// <summary>
-        /// The links to other objects related to this object.
-        /// </summary>
-        public ReferenceLinks Links
-        {
-            get
-            {
-                if (m_links == null)
-                {
-                    m_links = new ReferenceLinks();
-                }
-                return m_links;
-            }
-        }
-
-        [DataMember(Name = "_links", EmitDefaultValue = false)]
-        private ReferenceLinks m_links;
     }
 }
