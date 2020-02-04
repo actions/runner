c241703e18c215590ff6013726bb51a915c8e072
Thomas Boop <52323235+thboop@users.noreply.github.com>
2020-02-04 11:03:22 -0500

    Update dev.cmd

--- src/dev.cmd
+++ src/dev.cmd
@@ -2,13 +2,13 @@
 @echo off
  rem add expected utils to path
 IF EXIST C:\Program Files\Git\usr\bin (
-  SET PATH=%PATH%;C:\Program Files\Git\usr\bin
+  SET PATH=;C:\Program Files\Git\usr\bin;%PATH%
 )
 IF EXIST C:\Program Files\Git\mingw64\bin (
-  SET PATH=%PATH%;C:\Program Files\Git\mingw64\bin
+  SET PATH=C:\Program Files\Git\mingw64\bin;%PATH%
 )
 IF EXIST C:\Program Files\Git\bin (
-  SET PATH=%PATH%;C:\Program Files\Git\bin
+  SET PATH=C:\Program Files\Git\bin;%PATH%
 )
 
  rem Check if SH_PATH is defined.
