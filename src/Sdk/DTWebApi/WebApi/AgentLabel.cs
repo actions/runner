826f58682e6c774dab9fbcff713a59bc039a20c7
Lokesh Gopu <lokesh755@github.com>
2020-04-13 21:33:13 -0400

    Added support for custom labels (#414)
    * Added support for custom labels
    
    * ignore case
    
    * Added interactive config for labels
    
    * Fixing L0s
    
    * pr comments

---
+++ src/Sdk/DTWebApi/WebApi/AgentLabel.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace GitHub.DistributedTask.WebApi
+{
+    [DataContract]
+    public class AgentLabel
+    {
+        [JsonConstructor]
+        public AgentLabel()
+        {
+        }
+
+        public AgentLabel(string name)
+        {
+            this.Name = name;
+            this.Type = LabelType.System;
+        }
+
+        public AgentLabel(string name, LabelType type)
+        {
+            this.Name = name;
+            this.Type = type;
+        }
+
+        private AgentLabel(AgentLabel labelToBeCloned)
+        {
+            this.Id = labelToBeCloned.Id;
+            this.Name = labelToBeCloned.Name;
+            this.Type = labelToBeCloned.Type;
+        }
+
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public LabelType Type
+        {
+            get;
+            set;
+        }
+
+        public AgentLabel Clone()
+        {
+            return new AgentLabel(this);
+        }
+    }
+}
---
+++ src/Sdk/DTWebApi/WebApi/LabelType.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace GitHub.DistributedTask.WebApi
+{
+    [DataContract]
+    public enum LabelType
+    {
+        [EnumMember]
+        System = 0,
+
+        [EnumMember]
+        User = 1
+    }
+}
