d794de83eeb814fa14a5d96e9670b54e4325c8fa
jeffrey-kinesso <jeffrey.tremblay@kinesso.com>
2020-07-18 10:12:30 -0400

    dbl quotes around variable so CD works if path contains spaces

--- src/Misc/layoutroot/config.sh
+++ src/Misc/layoutroot/config.sh
@@ -67,7 +67,7 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-cd $DIR
+cd "$DIR"
 
 source ./env.sh
 
