00f6b43f842987f5be402e33a84687bdb339b6b4
Sergii Shymko <sergey@shymko.net>
2020-12-16 17:42:29 -0800

    Introduce function count() for arrays & strings

---
+++ src/Sdk/DTExpressions2/Expressions2/Sdk/Functions/Count.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GitHub.DistributedTask.Expressions2.Sdk.Functions
+{
+    internal sealed class Count : Function
+    {
+        protected sealed override Boolean TraceFullyRealized => false;
+
+        protected sealed override Object EvaluateCore(
+            EvaluationContext context,
+            out ResultMemory resultMemory)
+        {
+            resultMemory = null;
+            var items = Parameters[0].Evaluate(context);
+            
+            // Array
+            if (items.TryGetCollectionInterface(out var collection) &&
+                collection is IReadOnlyArray array)
+            {
+                return array.Count;
+            }
+            // Primitive
+            else if (items.IsPrimitive)
+            {
+                return items.ConvertToString().Length;
+            }
+            // Otherwise return zero
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
