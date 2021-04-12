0fb551c67580239443fc82226070537464f68d61
eric sciple <ericsciple@github.com>
2021-04-12 16:34:40 -0500

    minor cleanup in composite

--- src/Runner.Worker/StepsContext.cs
+++ src/Runner.Worker/StepsContext.cs
@@ -10,11 +10,19 @@ using GitHub.Runner.Sdk;
 
 namespace GitHub.Runner.Worker
 {
+    /// <summary>
+    /// Manages the "steps" context. The "steps" context is used to track individual steps
+    /// "outcome", "conclusion", and "outputs".
+    /// </summary>
     public sealed class StepsContext
     {
         private static readonly Regex _propertyRegex = new Regex("^[a-zA-Z_][a-zA-Z0-9_]*$", RegexOptions.Compiled);
         private readonly DictionaryContextData _contextData = new DictionaryContextData();
 
+        /// <summary>
+        /// Clears memory for a composite action's isolated "steps" context, after the action
+        /// is finished executing.
+        /// </summary>
         public void ClearScope(string scopeName)
         {
             if (_contextData.TryGetValue(scopeName, out _))
@@ -23,6 +31,14 @@ namespace GitHub.Runner.Worker
             } 
         }
 
+        /// <summary>
+        /// Gets the "steps" context for a given scope. The root steps in a workflow use the
+        /// default "steps" context (i.e. scopeName="").
+        ///
+        /// An isolated "steps" context is created for each composite action. All child steps
+        /// within a composite action, share an isolated "steps" context. The scope name matches
+        /// the composite action's fully qualified context name.
+        /// </summary>
         public DictionaryContextData GetScope(string scopeName)
         {
             if (scopeName == null)
