c83fdd365e03537f4a7d7d94035a82869b503b1d
Ian Walter <pub@ianwalter.dev>
2021-05-13 16:04:07 -0400

    Updating externals.sh

--- src/Misc/externals.sh
+++ src/Misc/externals.sh
@@ -4,6 +4,7 @@ PRECACHE=$2
 
 NODE_URL=https://nodejs.org/dist
 NODE12_VERSION="12.13.1"
+NODE14_VERSION="14.17.0"
 
 get_abs_path() {
   # exploits the fact that pwd will print abs path when no args
@@ -126,6 +127,8 @@ function acquireExternalTool() {
 if [[ "$PACKAGERUNTIME" == "win-x64" || "$PACKAGERUNTIME" == "win-x86" ]]; then
     acquireExternalTool "$NODE_URL/v${NODE12_VERSION}/$PACKAGERUNTIME/node.exe" node12/bin
     acquireExternalTool "$NODE_URL/v${NODE12_VERSION}/$PACKAGERUNTIME/node.lib" node12/bin
+    acquireExternalTool "$NODE_URL/v${NODE14_VERSION}/$PACKAGERUNTIME/node.exe" node14/bin
+    acquireExternalTool "$NODE_URL/v${NODE14_VERSION}/$PACKAGERUNTIME/node.lib" node14/bin
     if [[ "$PRECACHE" != "" ]]; then
         acquireExternalTool "https://github.com/microsoft/vswhere/releases/download/2.6.7/vswhere.exe" vswhere
     fi
@@ -134,18 +137,23 @@ fi
 # Download the external tools only for OSX.
 if [[ "$PACKAGERUNTIME" == "osx-x64" ]]; then
     acquireExternalTool "$NODE_URL/v${NODE12_VERSION}/node-v${NODE12_VERSION}-darwin-x64.tar.gz" node12 fix_nested_dir
+    acquireExternalTool "$NODE_URL/v${NODE14_VERSION}/node-v${NODE14_VERSION}-darwin-x64.tar.gz" node14 fix_nested_dir
 fi
 
 # Download the external tools for Linux PACKAGERUNTIMEs.
 if [[ "$PACKAGERUNTIME" == "linux-x64" ]]; then
     acquireExternalTool "$NODE_URL/v${NODE12_VERSION}/node-v${NODE12_VERSION}-linux-x64.tar.gz" node12 fix_nested_dir
     acquireExternalTool "https://vstsagenttools.blob.core.windows.net/tools/nodejs/${NODE12_VERSION}/alpine/x64/node-${NODE12_VERSION}-alpine-x64.tar.gz" node12_alpine
+    acquireExternalTool "$NODE_URL/v${NODE14_VERSION}/node-v${NODE14_VERSION}-linux-x64.tar.gz" node14 fix_nested_dir
+    acquireExternalTool "https://unofficial-builds.nodejs.org/download/release/v${NODE14_VERSION}/node-v${NODE14_VERSION}-linux-x64-musl.tar.xz" node14_alpine
 fi
 
 if [[ "$PACKAGERUNTIME" == "linux-arm64" ]]; then
     acquireExternalTool "$NODE_URL/v${NODE12_VERSION}/node-v${NODE12_VERSION}-linux-arm64.tar.gz" node12 fix_nested_dir
+    acquireExternalTool "$NODE_URL/v${NODE14_VERSION}/node-v${NODE14_VERSION}-linux-arm64.tar.gz" node14 fix_nested_dir
 fi
 
 if [[ "$PACKAGERUNTIME" == "linux-arm" ]]; then
     acquireExternalTool "$NODE_URL/v${NODE12_VERSION}/node-v${NODE12_VERSION}-linux-armv7l.tar.gz" node12 fix_nested_dir
+    acquireExternalTool "$NODE_URL/v${NODE14_VERSION}/node-v${NODE14_VERSION}-linux-armv7l.tar.gz" node14 fix_nested_dir
 fi
