911566dbab097681146b7d649a9e2b30ddc155dc
Ferenc Hammerl <fhammerl@github.com>
2021-05-20 14:30:45 +0200

    Wait for debugger to attach in Worker
    Only if GITHUB_ACTIONS_RUNNER_ATTACH_DEBUGGER is set

--- src/Runner.Worker/Program.cs
+++ src/Runner.Worker/Program.cs
@@ -1,9 +1,9 @@
-﻿using GitHub.Runner.Common.Util;
-using System;
+﻿using System;
 using System.Globalization;
 using System.Threading.Tasks;
 using GitHub.Runner.Common;
 using GitHub.Runner.Sdk;
+using System.Diagnostics;
 
 namespace GitHub.Runner.Worker
 {
@@ -19,11 +19,16 @@ namespace GitHub.Runner.Worker
 
         public static async Task<int> MainAsync(IHostContext context, string[] args)
         {
+            Tracing trace = context.GetTrace(nameof(GitHub.Runner.Worker));
+            if (bool.TryParse(Environment.GetEnvironmentVariable("GITHUB_ACTIONS_RUNNER_ATTACH_DEBUGGER"), out _))
+            {
+                await WaitForDebugger(trace);
+            }
+
             // We may want to consider registering this handler in Worker.cs, similiar to the unloading/SIGTERM handler
             //ITerminal registers a CTRL-C handler, which keeps the Runner.Worker process running
             //and lets the Runner.Listener handle gracefully the exit.
             var term = context.GetService<ITerminal>();
-            Tracing trace = context.GetTrace(nameof(GitHub.Runner.Worker));
             try
             {
                 trace.Info($"Version: {BuildConstants.RunnerPackage.Version}");
@@ -63,6 +68,25 @@ namespace GitHub.Runner.Worker
             }
 
             return 1;
+        }
+
+
+
+        /// <summary>
+        /// Runner.Worker is started by Runner.Listener in a separate process,
+        /// so the two can't be debugged in the same session.
+        /// This method halts the Runner.Worker process until a debugger is attached,
+        /// allowing a developer to debug Runner.Worker from start to finish.
+        /// </summary>
+        private static async Task WaitForDebugger(Tracing trace)
+        {
+            trace.Info($"Waiting for a debugger to be attached. Edit the 'GITHUB_ACTIONS_RUNNER_ATTACH_DEBUGGER' environment variable to toggle this feature.");
+            while (!Debugger.IsAttached)
+            {
+                trace.Info($"Waiting for a debugger to be attached");
+                await Task.Delay(1000);
+            }
+            Debugger.Break();
         }
     }
 }
