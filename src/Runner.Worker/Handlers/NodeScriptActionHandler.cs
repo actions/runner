a4afe2a5ff759cfae7125b0d1ee5e4d458864cf2
Ferenc Hammerl <31069338+fhammerl@users.noreply.github.com>
2021-06-24 18:02:26 +0200

    Add generateIdTokenUrl to env vars

--- src/Runner.Worker/Handlers/NodeScriptActionHandler.cs
+++ src/Runner.Worker/Handlers/NodeScriptActionHandler.cs
@@ -53,6 +53,10 @@ namespace GitHub.Runner.Worker.Handlers
             {
                 Environment["ACTIONS_CACHE_URL"] = cacheUrl;
             }
+            if (systemConnection.Data.TryGetValue("GenerateIdTokenUrl", out var generateIdTokenUrl) && !string.IsNullOrEmpty(generateIdTokenUrl))
+            {
+                Environment["ACTIONS_ID_TOKEN_REQUEST_URL"] = generateIdTokenUrl;
+            }
 
             // Resolve the target script.
             string target = null;
