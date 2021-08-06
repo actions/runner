5eba977a0471a85051317d7a6819743196c7c198
Christopher Homberger <christopher.homberger@web.de>
2021-08-06 21:52:06 +0200

    Log token validation errors

--- src/Runner.Server/Controllers/AuthController.cs
+++ src/Runner.Server/Controllers/AuthController.cs
@@ -31,6 +31,7 @@ namespace Runner.Server.Controllers {
             var myAudience = "http://githubactionsserver";
 
             var tokenHandler = new JwtSecurityTokenHandler();
+            var now = DateTime.Now.ToString();
             try
             {
                 tokenHandler.ValidateToken(token, new TokenValidationParameters
@@ -43,35 +44,33 @@ namespace Runner.Server.Controllers {
                     IssuerSigningKey = new RsaSecurityKey(rsa)
                 }, out SecurityToken validatedToken);
             }
+            catch (SecurityTokenNotYetValidException ex)
+            {
+                var nbf = ex.NotBefore.ToString();
+                Console.WriteLine($"Security Token not valid yet: {nbf}, now {now}");
+                return false;
+            }
+            catch (SecurityTokenExpiredException ex) {
+                var exp = ex.Expires.ToString();
+                Console.WriteLine($"Security Token expired: {exp}, now {now}");
+                return false;
+            }
             catch
             {
+                Console.WriteLine($"Security Token is invalid");
                 return false;
             }
             return true;
         }
         [HttpPost]
-        public Task<FileStreamResult> Authenticate() {
-            // var ua = Request.Headers["User-Agent"].ToArray()[0];
-            // var ridoffset = ua.IndexOf("RunnerId/");
-            // if(ridoffset != -1) {
-            //     ridoffset += "RunnerId/".Length;
-            // }
-            // var ridend = ua.IndexOf(' ', ridoffset);
-            // var agentid = Guid.Parse(ua.Substring(ridoffset, ridend - ridoffset));
+        public async Task<IActionResult> Authenticate() {
             var token2 = new JwtSecurityToken(Request.Form["client_assertion"]);
             var id = token2.Claims.Where(c => c.Type == "sub").FirstOrDefault();
             var agentid = Guid.Parse(id.Value);
-            // "GitHubActionsRunner-linux-arm64/3.0.3 RunnerId/2caee715-9385-4e48-af19-755bb886f814 (Linux 5.4.0-1025-raspi #28-Ubuntu SMP PREEMPT Wed Dec 9 17:10:53 UTC 2020) VSServices/3.0.3.0 (NetStandard; Linux 5.4.0-1025-raspi #28-Ubuntu SMP PREEMPT Wed Dec 9 17:10:53 UTC 2020)"
-            //Request.Form["grant_type"] == "client_credentials"
-            //Request.Form["client_assertion_type"] == "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
-            //Request.Form["client_assertion"]
-            // var assertion = this.Request.Form["Assertion"];
-            // var token = assertion.ToArray()[0];
 
-            var ret = db.Agents.Include(a => a.TaskAgent).Include(a => a.TaskAgent.Labels).Include(a => a.Pool).Where(a => a.ClientId == agentid).FirstOrDefault();
-            
-            if(!ValidateCurrentToken(ret.PublicKey, Request.Form["client_assertion"])) {
-                return null;
+            var ret = db.Agents.Where(a => a.ClientId == agentid).Include(a => a.TaskAgent).Include(a => a.TaskAgent.Labels).Include(a => a.Pool).FirstOrDefault();
+            if(ret == null || !ValidateCurrentToken(ret.PublicKey, Request.Form["client_assertion"])) {
+                return NotFound();
             }
 
             var mySecurityKey = new RsaSecurityKey(Startup.AccessTokenParameter);
@@ -93,11 +92,7 @@ namespace Runner.Server.Controllers {
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
             
-            // if(ValidateCurrentToken(token)) {
-                // await HttpContext.GetTokenAsync("bearer", "test");
-                return Ok(new VssOAuthTokenResponse(){AccessToken = tokenHandler.WriteToken(token), ExpiresIn = 7 * 24 * 60 * 60, Scope = "/", TokenType = "access_token" });
-            // }
-            // return NotFound();
+            return await Ok(new VssOAuthTokenResponse(){AccessToken = tokenHandler.WriteToken(token), ExpiresIn = 7 * 24 * 60 * 60, Scope = "/", TokenType = "access_token" });
         }
     }
 }
