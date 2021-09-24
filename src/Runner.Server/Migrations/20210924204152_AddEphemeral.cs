890fd10e544f05ebaf4b4f24737f7d9dc00a7bf1
Christopher Homberger <christopher.homberger@web.de>
2021-09-24 22:43:39 +0200

    Update Database Model

---
+++ src/Runner.Server/Migrations/20210924204152_AddEphemeral.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Runner.Server.Migrations
+{
+    public partial class AddEphemeral : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Ephemeral",
+                table: "TaskAgentReference",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ephemeral",
+                table: "TaskAgentReference");
+        }
+    }
+}
