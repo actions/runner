c96a53e496470b20292ee3ba616d5ddd91df5a8e
TingluoHuang <TingluoHuang@github.com>
2021-11-12 15:45:35 -0500

    .

--- src/Sdk/Common/Common/VssHttpRequestSettings.cs
+++ src/Sdk/Common/Common/VssHttpRequestSettings.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
+using GitHub.Services.Common;
 
 namespace GitHub.Services.Common
 {
@@ -291,7 +292,7 @@ namespace GitHub.Services.Common
         protected internal virtual Boolean ApplyTo(HttpRequestMessage request)
         {
             // Make sure we only apply the settings to the request once
-            if (request.Options.TryGetValue(new HttpRequestOptionsKey<VssHttpRequestSettings>(PropertyName), out _))
+            if (request.Options.TryGetValue<object>(PropertyName, out _))
             {
                 return false;
             }
