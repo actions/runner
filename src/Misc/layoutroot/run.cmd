c850c8ce6198c62a0d95e18e2d116defc0c1b3bc
Ferenc Hammerl <hammerl.ferenc@gmail.com>
2021-11-29 15:12:59 +0100

    Relaunch the listener without quitting run.cmd

--- src/Misc/layoutroot/run.cmd
+++ src/Misc/layoutroot/run.cmd
@@ -13,21 +13,43 @@ if defined VERBOSE_ARG (
 rem Unblock files in the root of the layout folder. E.g. .cmd files.
 powershell.exe -NoLogo -Sta -NoProfile -NonInteractive -ExecutionPolicy Unrestricted -Command "$VerbosePreference = %VERBOSE_ARG% ; Get-ChildItem -LiteralPath '%~dp0' | ForEach-Object { Write-Verbose ('Unblock: {0}' -f $_.FullName) ; $_ } | Unblock-File | Out-Null"
 
-if /i "%~1" equ "localRun" (
-    rem ********************************************************************************
-    rem Local run.
-    rem ********************************************************************************
-    "%~dp0bin\Runner.Listener.exe" %*
-) else (
-  rem ********************************************************************************
-  rem Run.
-  rem ********************************************************************************
-  "%~dp0bin\Runner.Listener.exe" run %*
 
-  rem Return code 4 means the run once runner received an update message.
-  rem Sleep 5 seconds to wait for the update process finish and run the runner again.
-  if ERRORLEVEL 4 (
-    timeout /t 5 /nobreak > NUL
-    "%~dp0bin\Runner.Listener.exe" run %*
-  )
+rem ********************************************************************************
+rem Run.
+rem ********************************************************************************
+
+:relaunch_listener
+"%~dp0bin\Runner.Listener.exe" run %*
+
+rem using `if %ERRORLEVEL% EQU N` insterad of `if ERRORLEVEL N`
+rem `if ERRORLEVEL N` means: error level is N or MORE
+  
+if %ERRORLEVEL% EQU 0 (
+  echo "Runner listener exit with 0 return code, stop the service, no retry needed."
+  exit %ERRORLEVEL%
+)
+
+if %ERRORLEVEL% EQU 1 (
+  echo "Runner listener exit with terminated error, stop the service, no retry needed."
+  exit %ERRORLEVEL%
+)
+
+if %ERRORLEVEL% EQU 2 (
+  echo "Runner listener exit with retryable error, re-launch runner in 5 seconds."
+  timeout /t 5 /nobreak > NUL
+  goto :launch_listener
+)
+
+rem Return code 4 means the runner received an update message.
+if %ERRORLEVEL% EQU 3 (
+  echo "Runner listener exit because of updating, re-launch runner in 5 seconds"
+  timeout /t 5 /nobreak > NUL
+  goto :launch_listener
+)
+
+rem Return code 4 means the ephemeral runner received an update message.
+if %ERRORLEVEL% EQU 4 (
+  echo "Runner listener exit because of updating, re-launch ephemeral runner in 5 seconds"
+  timeout /t 5 /nobreak > NUL
+  goto :launch_listener
 )
