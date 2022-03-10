589f8737e43ac5acb282e9aefd1e50e820368572
Ferenc Hammerl <31069338+fhammerl@users.noreply.github.com>
2022-03-10 14:08:21 +0000

    Allow script post steps

--- src/Runner.Worker/Handlers/ScriptHandler.cs
+++ src/Runner.Worker/Handlers/ScriptHandler.cs
@@ -24,12 +24,6 @@ namespace GitHub.Runner.Worker.Handlers
 
         protected override void PrintActionDetails(ActionRunStage stage)
         {
-
-            if (stage == ActionRunStage.Post)
-            {
-                throw new NotSupportedException("Script action should not have 'Post' job action.");
-            }
-
             Inputs.TryGetValue("script", out string contents);
             contents = contents ?? string.Empty;
             if (Action.Type == Pipelines.ActionSourceType.Script)
@@ -132,11 +126,6 @@ namespace GitHub.Runner.Worker.Handlers
 
         public async Task RunAsync(ActionRunStage stage)
         {
-            if (stage == ActionRunStage.Post)
-            {
-                throw new NotSupportedException("Script action should not have 'Post' job action.");
-            }
-
             // Validate args
             Trace.Entering();
             ArgUtil.NotNull(ExecutionContext, nameof(ExecutionContext));
