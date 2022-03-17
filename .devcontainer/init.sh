d6a244fc25fd5b10769fa241a982b7e9a726b40a
Ferenc Hammerl <31069338+fhammerl@users.noreply.github.com>
2022-03-17 21:23:19 +0000

    No need for service restore

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,13 +1,8 @@
-### Create _dotnetsdk
-echo 'Creating symlink for embedded sdk...'
-ln -s /home/codespace/.dotnet/sdk/ _dotnetsdk 
-echo .6.0.100 >> _dotnetsdk/6.0.100/.6.0.100 
-ln -s /home/codespace/.dotnet/dotnet _dotnetsdk/6.0.100/dotnet
+
 
 ### Restore 
 dotnet restore src/Runner.Listener &
 dotnet restore src/Runner.Common &
 dotnet restore src/Runner.Sdk &
 dotnet restore src/Runner.Worker &
-dotnet restore src/Runner.Service &
 dotnet restore src/Test &
