6f3fc3fdf7724c01a10f8989fe401b2bf6afa72e
Ferenc Hammerl <fhammerl@github.com>
2022-03-24 06:15:07 -0700

    Refactor dockerhookcommandmanager

--- src/Runner.Worker/Container/DockerHookCommandManager.cs
+++ src/Runner.Worker/Container/DockerHookCommandManager.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using GitHub.DistributedTask.WebApi;
 using GitHub.Runner.Common;
 using GitHub.Runner.Common.Util;
+using GitHub.Runner.Sdk;
 using GitHub.Runner.Worker.Handlers;
 using GitHub.Services.WebApi;
 
@@ -19,10 +21,13 @@ namespace GitHub.Runner.Worker.Container
     {
         public override async Task<int> DockerStart(IExecutionContext context, string containerId)
         {
-            // check for env var
-            // execute script
-            // Create the handler data.
-            var path = GetDockerHook(nameof(DockerStart));
+            // TODO: do we want this flag to come from the .yml as well?
+            if (!StringUtil.ConvertToBoolean(Environment.GetEnvironmentVariable("GITHUB_ACTIONS_ALLOW_DOCKER_OVERRIDE")))
+            {
+                return await base.DockerStart(context, containerId);
+            }
+
+            var path = GetDockerHookFileName(nameof(DockerStart));
             var scriptDirectory = Path.GetDirectoryName(path);
             var stepHost = HostContext.CreateService<IDefaultStepHost>();
             var prependPath = string.Join(Path.PathSeparator.ToString(), context.Global.PrependPath.Reverse<string>());
@@ -45,14 +50,12 @@ namespace GitHub.Runner.Worker.Container
                             context.Global.Variables,
                             actionDirectory: scriptDirectory,
                             localActionContainerSetupSteps: null) as ScriptHandler;
-            handler.PrepareExecution(ActionRunStage.Main); // TODO: find out stage, we only use Start in pre, but double check
-
-            await handler.RunAsync(ActionRunStage.Main);
-
-            return ((int?)handler.ExecutionContext.CommandResult) ?? 0;
+            handler.PrepareExecution(ActionRunStage.Pre); // TODO: find out stage, we only use Start in pre, but double check
+            await handler.RunAsync(ActionRunStage.Pre);
+            return handler.ExecutionContext.CommandResult == TaskResult.Succeeded ? 0 : 1;
         }
 
-        private string GetDockerHook(string commandName)
+        private string GetDockerHookFileName(string commandName)
         {
             commandName = string.Format("{0}.sh", commandName.ToLower());
             return Path.Combine(HostContext.GetDirectory(WellKnownDirectory.DockerHooks), commandName);
