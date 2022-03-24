59e038a3a62e591a45a760fa6c45e33419df8b7e
Ferenc Hammerl <fhammerl@github.com>
2022-03-24 06:08:41 -0700

    Remove unused dependencies

--- src/Runner.Worker/Handlers/StepHost.cs
+++ src/Runner.Worker/Handlers/StepHost.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Text;
 using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
-using GitHub.DistributedTask.WebApi;
-using GitHub.Runner.Common.Util;
 using GitHub.Runner.Worker.Container;
-using GitHub.Services.WebApi;
-using Newtonsoft.Json;
 using GitHub.Runner.Common;
 using GitHub.Runner.Sdk;
 using System.Linq;
