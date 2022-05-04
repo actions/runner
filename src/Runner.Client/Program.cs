1e11c03593e3aa602e55f96fbac274bbb8c44f1d
Christopher Homberger <christopher.homberger@web.de>
2022-05-04 16:43:51 +0200

    Workflow finish prefer colored Job finish design

--- src/Runner.Client/Program.cs
+++ src/Runner.Client/Program.cs
@@ -1464,7 +1464,12 @@ namespace Runner.Client
                                             }
                                             if(line == "event: workflow") {
                                                 var _workflow = JsonConvert.DeserializeObject<WorkflowEventArgs>(data);
-                                                Console.WriteLine($"Workflow {_workflow.runid} finished with status {(_workflow.Success ? "Success" : "Failure")}");
+                                                var rec = (from r in timelineRecords where r.Value.TimeLine?[0]?.Id == r.Key select r.Value).FirstOrDefault();
+                                                if(rec != null) {
+                                                    WriteLogLine((int)rec.Color, $"{(rec.WorkflowName != null ? $"{rec.WorkflowName} / " : "")}{rec.TimeLine[0].Name}", $"Workflow {_workflow.runid} Completed with Status: {(_workflow.Success ? "Success" : "Failure")}");
+                                                } else {
+                                                    Console.WriteLine($"Workflow {_workflow.runid} finished with status {(_workflow.Success ? "Success" : "Failure")}");
+                                                }
                                                 hasErrors |= !_workflow.Success;
                                                 hasAny = true;
                                             }
