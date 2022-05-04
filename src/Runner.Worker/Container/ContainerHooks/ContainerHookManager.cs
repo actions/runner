3e6ebe709b591f3292b143eacd97bb63844b91ae
Stefan Ruvceski <stefan.ruvceski@akvelon.com>
2022-05-04 17:11:22 +0200

    Port mappings saved after creating services

--- src/Runner.Worker/Container/ContainerHooks/ContainerHookManager.cs
+++ src/Runner.Worker/Container/ContainerHooks/ContainerHookManager.cs
@@ -93,11 +93,13 @@ namespace GitHub.Runner.Worker.Container.ContainerHooks
                 };
 
                 // TODO: workout port mappings + format
-                // foreach (var portMapping in containerInfo.UserPortMappings)
-                // {
-                //     // TODO: currently the format is ports["80:8080"] = "80:8080", fix this?
-                //     (service["ports"] as DictionaryContextData)[$"{portMapping.Key}:{portMapping.Value}"] = new StringContextData($"{portMapping.Key}:{portMapping.Value}");
-                // }
+                container.PortMappings = new Dictionary<string, string>();
+                foreach (var portMapping in containerInfo.UserPortMappings)
+                {
+                    // TODO: currently the format is ports["80:8080"] = "80:8080", fix this?
+                    (service["ports"] as DictionaryContextData)[$"{portMapping.Key}:{portMapping.Value}"] = new StringContextData($"{portMapping.Key}:{portMapping.Value}");
+                    container.PortMappings.Add(portMapping);
+                }
                 context.JobContext.Services[containerInfo.ContainerNetworkAlias] = service;
             }
         }
