3fbba34ea98cf9084ba5656b1f2c1160d4c87302
Stefan Ruvceski <stefan.ruvceski@akvelon.com>
2022-05-04 12:16:49 +0200

    Get ContainerRuntimePath for JobContainers from hooks

--- src/Runner.Worker/Container/ContainerHooks/ContainerHookManager.cs
+++ src/Runner.Worker/Container/ContainerHooks/ContainerHookManager.cs
@@ -27,10 +27,12 @@ namespace GitHub.Runner.Worker.Container.ContainerHooks
     {
         private const string ResponseFolderName = "_hook_responses";
         private string HookIndexPath;
+        private IDockerCommandManager _dockerManager;
         public override void Initialize(IHostContext hostContext)
         {
             base.Initialize(hostContext);
             HookIndexPath = $"{Environment.GetEnvironmentVariable(Constants.Hooks.ContainerHooksPath)}";
+            _dockerManager = HostContext.GetService<IDockerCommandManager>();
         }
 
         public async Task PrepareJobAsync(IExecutionContext context, List<ContainerInfo> containers)
@@ -71,10 +73,9 @@ namespace GitHub.Runner.Worker.Container.ContainerHooks
 
             SaveHookState(context, response.State);
 
-            // TODO: figure out if we need ContainerRuntimePath for anything
-            // var configEnvFormat = "--format \"{{range .Config.Env}}{{println .}}{{end}}\"";
-            // var containerEnv = await _dockerManager.DockerInspect(executionContext, container.ContainerId, configEnvFormat);
-            // container.ContainerRuntimePath = DockerUtil.ParsePathFromConfigEnv(containerEnv);
+            var configEnvFormat = "--format \"{{range .Config.Env}}{{println .}}{{end}}\"";
+            var containerEnv = await _dockerManager.DockerInspect(context, jobContainer?.ContainerId, configEnvFormat);
+            jobContainer.ContainerRuntimePath = DockerUtil.ParsePathFromConfigEnv(containerEnv);
 
             for (var i = 0; i < response?.Context?.Services?.Count; i++)
             {
