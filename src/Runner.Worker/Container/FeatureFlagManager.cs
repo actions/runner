d5ceff0465fded68dd641ef65a8909e6275796fe
Ferenc Hammerl <fhammerl@github.com>
2022-05-18 05:15:23 -0700

    Fail if compatible hookfile not found

--- src/Runner.Worker/Container/FeatureFlagManager.cs
+++ src/Runner.Worker/Container/FeatureFlagManager.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using GitHub.Runner.Common;
 
 namespace GitHub.Runner.Worker.Container
@@ -7,8 +9,23 @@ namespace GitHub.Runner.Worker.Container
     {
         public static bool IsContainerHooksEnabled(IExecutionContext executionContext)
         {
-            return string.IsNullOrEmpty(Environment.GetEnvironmentVariable(Constants.Hooks.ContainerHooksPath)) && 
-            ((executionContext.Global?.Variables.GetBoolean(Constants.Runner.Features.AllowRunnerContainerHooks)) ?? false);
+            var hookExecutablePath = Environment.GetEnvironmentVariable(Constants.Hooks.ContainerHooksPath);
+            var isContainerHookFeatureFlagSet = executionContext.Global?.Variables.GetBoolean(Constants.Runner.Features.AllowRunnerContainerHooks) ?? false;
+
+            if (isContainerHookFeatureFlagSet && !string.IsNullOrEmpty(hookExecutablePath))
+            {
+                if (!File.Exists(hookExecutablePath))
+                {
+                    throw new Exception($"File not found at '{hookExecutablePath}'. Set {Constants.Hooks.ContainerHooksPath} to the path of an existing file.");
+                }
+                var supportedHookExtensions = new string[] { ".js", ".sh", ".ps1" };
+                if (!supportedHookExtensions.Any(extension => hookExecutablePath.EndsWith(extension)))
+                {
+                    throw new Exception($"Invalid file extension at '{hookExecutablePath}'. {Constants.Hooks.ContainerHooksPath} must be a path to a file with one of the following extensions: {string.Join(", ", supportedHookExtensions)}");
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
