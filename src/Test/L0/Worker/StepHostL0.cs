87e14aef63d27e77ad72a6d774f1ba70e18e0bd3
ruvceskistefan <96768603+ruvceskistefan@users.noreply.github.com>
2022-06-01 11:39:09 +0200

    small code refactoring
    Co-authored-by: Ferenc Hammerl <31069338+fhammerl@users.noreply.github.com>

--- src/Test/L0/Worker/StepHostL0.cs
+++ src/Test/L0/Worker/StepHostL0.cs
@@ -55,7 +55,6 @@ namespace GitHub.Runner.Common.Tests.Worker
                 Assert.Equal("node12", nodeVersion);
             }
         }
-        
         [Fact]
         [Trait("Level", "L0")]
         [Trait("Category", "Worker")]
