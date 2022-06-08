6082a011f68d9ff691a1f37bdaf4d9d5f3797b9c
Ferenc Hammerl <fhammerl@github.com>
2022-06-08 01:18:03 -0700

    Fix flipped condition

--- src/Runner.Worker/FeatureManager.cs
+++ src/Runner.Worker/FeatureManager.cs
@@ -10,7 +10,7 @@ namespace GitHub.Runner.Worker
         public static bool IsContainerHooksEnabled(Variables variables)
         {
             var isContainerHookFeatureFlagSet = variables?.GetBoolean(Constants.Runner.Features.AllowRunnerContainerHooks) ?? true;
-            var isContainerHooksPathSet = string.IsNullOrEmpty(Environment.GetEnvironmentVariable(Constants.Hooks.ContainerHooksPath));
+            var isContainerHooksPathSet = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(Constants.Hooks.ContainerHooksPath));
             return isContainerHookFeatureFlagSet && isContainerHooksPathSet;
         }
     }
