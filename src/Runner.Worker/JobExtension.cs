bc20530cc3492ff8825921069d4d2e53b544e94f
Thomas Boop <52323235+thboop@users.noreply.github.com>
2022-06-09 16:22:07 -0400

    Update src/Runner.Worker/JobExtension.cs

--- src/Runner.Worker/JobExtension.cs
+++ src/Runner.Worker/JobExtension.cs
@@ -679,7 +679,7 @@ namespace GitHub.Runner.Worker
         {
             if (StringUtil.ConvertToBoolean(Environment.GetEnvironmentVariable(Constants.Variables.Actions.RequireJobContainer)) && container == null)
             {
-                throw new ArgumentException("Jobs without a job container are forbidden on this  runner, please add a 'container:' to your job or contact your self-hosted runner administrator.");
+                throw new ArgumentException("Jobs without a job container are forbidden on this runner, please add a 'container:' to your job or contact your self-hosted runner administrator.");
             }
         }
     }
