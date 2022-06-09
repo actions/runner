8d2afab7e9aa7af1591eed8faffb4ff7be78e7e8
Ferenc Hammerl <fhammerl@github.com>
2022-06-09 05:29:52 -0700

    Better exception types

--- src/Runner.Worker/Container/ContainerHooks/ContainerHookManager.cs
+++ src/Runner.Worker/Container/ContainerHooks/ContainerHookManager.cs
@@ -214,13 +214,13 @@ namespace GitHub.Runner.Worker.Container.ContainerHooks
         {
             if (!string.IsNullOrEmpty(HookIndexPath) && !File.Exists(HookIndexPath))
             {
-                throw new Exception($"File not found at '{HookIndexPath}'. Set {Constants.Hooks.ContainerHooksPath} to the path of an existing file.");
+                throw new FileNotFoundException($"File not found at '{HookIndexPath}'. Set {Constants.Hooks.ContainerHooksPath} to the path of an existing file.");
             }
 
             var supportedHookExtensions = new string[] { ".js", ".sh", ".ps1" };
             if (!supportedHookExtensions.Any(extension => HookIndexPath.EndsWith(extension)))
             {
-                throw new Exception($"Invalid file extension at '{HookIndexPath}'. {Constants.Hooks.ContainerHooksPath} must be a path to a file with one of the following extensions: {string.Join(", ", supportedHookExtensions)}");
+                throw new ArgumentOutOfRangeException($"Invalid file extension at '{HookIndexPath}'. {Constants.Hooks.ContainerHooksPath} must be a path to a file with one of the following extensions: {string.Join(", ", supportedHookExtensions)}");
             }
         }
 
