89022b8d7d5c43dd2e1069ca17f20b98e14edff1
eric sciple <ericsciple@github.com>
2022-06-09 13:30:07 -0500

    .

--- src/Runner.Sdk/Util/VssUtil.cs
+++ src/Runner.Sdk/Util/VssUtil.cs
@@ -57,7 +57,10 @@ namespace GitHub.Runner.Sdk
                 settings.SendTimeout = TimeSpan.FromSeconds(Math.Min(Math.Max(httpRequestTimeoutSeconds, 100), 1200));
             }
 
-            settings.AllowAutoRedirect = true;
+            if (StringUtil.ConvertToBoolean(Environment.GetEnvironmentVariable("GITHUB_ACTIONS_RUNNER_ALLOW_REDIRECT")))
+            {
+                settings.AllowAutoRedirect = true;
+            }
 
             // Remove Invariant from the list of accepted languages.
             //
