b957c3572718a20285b7b590f02c18371f6740dc
Nicholas Bergesen <nicholasbergesen@github.com>
2022-08-22 15:16:36 +0900

    Use group for inputs

--- src/Runner.Worker/JobExtension.cs
+++ src/Runner.Worker/JobExtension.cs
@@ -318,17 +318,20 @@ namespace GitHub.Runner.Worker
 
                     if (message.Variables.TryGetValue("system.workflowFileFullPath", out VariableValue workflowFileFullPath))
                     {
+                        context.Output($"uses: {workflowFileFullPath.Value}");
                         if (message.ContextData.TryGetValue("inputs", out var pipelineContextData)) {
                             var inputs = pipelineContextData.AssertDictionary("inputs");
                             if (inputs.Any()) {
+                                context.Output($"##[group] inputs");
                                 context.Output("with:");
                                 foreach(var input in inputs) {
                                     context.Output($"  {input.Key}: {input.Value}");
                                 }
+                                context.Output("##[endgroup]");
                             }
                         }
-                        
-                        if (!string.IsNullOrWhiteSpace(message.JobDisplayName)) {
+
+                         if (!string.IsNullOrWhiteSpace(message.JobDisplayName)) {
                             context.Output($"Complete job name: {message.JobDisplayName}");
                         }
                     }
