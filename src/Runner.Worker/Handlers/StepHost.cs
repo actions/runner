01fd04464d931699c6989f7c366a5ff3e155fe56
Nikola Jokic <97525037+nikola-jokic@users.noreply.github.com>
2022-08-23 07:42:29 -0700

    Escaping key and quoting it to avoid key based command injection (#2062)
    * escaping key and quoting it to avoid key based command injection
    
    * extracted creation of flags to DockerUtil, with testing included

--- src/Runner.Worker/Handlers/StepHost.cs
+++ src/Runner.Worker/Handlers/StepHost.cs
@@ -193,7 +193,7 @@ namespace GitHub.Runner.Worker.Handlers
                 TranslateToContainerPath(environment);
                 await containerHookManager.RunScriptStepAsync(context,
                                                                                    Container,
-                                                                                   workingDirectory,                                                                                   
+                                                                                   workingDirectory,
                                                                                    fileName,
                                                                                    arguments,
                                                                                    environment,
@@ -216,7 +216,7 @@ namespace GitHub.Runner.Worker.Handlers
             {
                 // e.g. -e MY_SECRET maps the value into the exec'ed process without exposing
                 // the value directly in the command
-                dockerCommandArgs.Add($"-e {env.Key}");
+                dockerCommandArgs.Add(DockerUtil.CreateEscapedOption("-e", env.Key));
             }
             if (!string.IsNullOrEmpty(PrependPath))
             {
