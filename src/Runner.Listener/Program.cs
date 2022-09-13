bd4894f7669957c4a79295ed5bdf401c2d444f57
Thomas Boop <thboop@github.com>
2022-09-13 09:52:44 -0400

    Add windows 11 check

--- src/Runner.Listener/Program.cs
+++ src/Runner.Listener/Program.cs
@@ -60,6 +60,17 @@ namespace GitHub.Runner.Listener
                         terminal.WriteLine("This runner version is built for Windows. Please install a correct build for your OS.");
                         return Constants.Runner.ReturnCode.TerminatedError;
                     }
+                    #if ARM64
+                        // A little hacky, but windows gives no way to differentiate between windows 10 and 11.
+                        // By default only 11 supports native x64 app emulation on arm, so we only want to support windows 11
+                        // https://docs.microsoft.com/en-us/windows/arm/overview#build-windows-apps-that-run-on-arm
+                        // TODO: change this to major version string once that doesn't return the same value for 10 and 11.
+                        if (Environment.OSVersion.Version.Build >= 22000;)
+                        {
+                            terminal.WriteLine("Win-arm64 runners require windows 11 or later. Please upgrade your operating system.");
+                            return Constants.Runner.ReturnCode.TerminatedError;
+                        }
+                    #endif
                     break;
                 default:
                     terminal.WriteLine($"Running the runner on this platform is not supported. The current platform is {RuntimeInformation.OSDescription} and it was built for {Constants.Runner.Platform.ToString()}.");
