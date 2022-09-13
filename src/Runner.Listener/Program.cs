503fa191a1263e7305e3534004f8294aace79aec
Thomas Boop <thboop@github.com>
2022-09-13 11:11:09 -0400

    fix typo

--- src/Runner.Listener/Program.cs
+++ src/Runner.Listener/Program.cs
@@ -65,7 +65,7 @@ namespace GitHub.Runner.Listener
                         // By default only 11 supports native x64 app emulation on arm, so we only want to support windows 11
                         // https://docs.microsoft.com/en-us/windows/arm/overview#build-windows-apps-that-run-on-arm
                         // TODO: change this to major version string once that doesn't return the same value for 10 and 11.
-                        if (Environment.OSVersion.Version.Build >= 22000;)
+                        if (Environment.OSVersion.Version.Build >= 22000)
                         {
                             terminal.WriteLine("Win-arm64 runners require windows 11 or later. Please upgrade your operating system.");
                             return Constants.Runner.ReturnCode.TerminatedError;
