252f4de577497a456b6dbdb92c89ef5d358be2e2
Yashwanth Anantharaju <yaananth@users.noreply.github.com>
2022-10-06 10:28:32 -0400

    changes to support specific run service URL (#2158)
    * changes to support run service url
    
    * feedback

--- src/Runner.Listener/RunnerJobRequestRef.cs
+++ src/Runner.Listener/RunnerJobRequestRef.cs
@@ -9,5 +9,7 @@ namespace GitHub.Runner.Listener
         public string Id { get; set; }
         [DataMember(Name = "runner_request_id")]
         public string RunnerRequestId { get; set; }
+        [DataMember(Name = "run_service_url")]
+        public string RunServiceUrl { get; set; }
     }
 }
