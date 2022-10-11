9ba99d7e348aa29d657089e50eea604c3d07af34
Ava Stancu <avastancu@github.com>
2022-10-11 10:09:24 +0000

    Avastancu/joannaakl/service container error log (#2110) (#2182)
    * Avastancu/joannaakl/service container error log (#2110)
    
    * adding support for a service container docker logs
    
    * Adding Unit test to ContainerOperationProvider
    
    * Adding another test to ContainerOperationProvider
    
    * placed the docker logs output in dedicated ##group section
    
    * Removed the exception thrown if the service container was not healthy
    
    * Removed duplicated logging to the executionContext
    
    * Updated the container logs sub-section message
    
    * Print service containers only if they were healthy
    Unhealthy service logs are printed in ContainerHealthCheckLogs called prior to this step.
    
    * Removed recently added method to inspect docker logs
    The method was doing the same thing as the existing DockerLogs method.
    
    * Added execution context error
    This will make a failed health check more visible in the UI without disrupting the execution of the program.
    
    * Removing the section 'Waiting for all services to be ready'
    
    Since nested subsections are not being displayed properly and we already need one subsection per service error.
    
    * Update src/Runner.Worker/Container/DockerCommandManager.cs
    
    Co-authored-by: Tingluo Huang <tingluohuang@github.com>
    
    * Update src/Test/L0/TestHostContext.cs
    
    Co-authored-by: Tingluo Huang <tingluohuang@github.com>
    
    * Change the logic for printing Service Containers logs
    
    Service container logs will be printed in the 'Start containers' section only if there is an error.
    Healthy services will have their logs printed in the 'Stop Containers' section.
    
    * Removed unused import
    
    * Added back section group.
    
    * Moved service containers error logs to separate group sections
    
    * Removed the test testing the old logic flow.
    
    * Remove unnecessary 'IsAnyUnhealthy' flag
    
    * Remove printHello() function
    
    * Add newline to TestHostContext
    
    * Remove unnecessary field 'UnhealthyContainers'
    
    * Rename boolean flag indicating service container failure
    
    * Refactor healthcheck logic to separate method to enable unit testing.
    
    * Remove the default value for bool variable
    
    * Update src/Runner.Worker/ContainerOperationProvider.cs
    
    Co-authored-by: Tingluo Huang <tingluohuang@github.com>
    
    * Update src/Runner.Worker/ContainerOperationProvider.cs
    
    Co-authored-by: Tingluo Huang <tingluohuang@github.com>
    
    * Rename Healthcheck back to ContainerHealthcheck
    
    * Make test sequential
    
    * Unextract the container error logs method
    
    * remove test asserting thrown exception
    
    * Add configure await
    
    * Update src/Test/L0/Worker/ContainerOperationProviderL0.cs
    
    Co-authored-by: Tingluo Huang <tingluohuang@github.com>
    
    * Update src/Test/L0/Worker/ContainerOperationProviderL0.cs
    
    Co-authored-by: Tingluo Huang <tingluohuang@github.com>
    
    * Update src/Test/L0/Worker/ContainerOperationProviderL0.cs
    
    Co-authored-by: Tingluo Huang <tingluohuang@github.com>
    
    * Update src/Test/L0/Worker/ContainerOperationProviderL0.cs
    
    Co-authored-by: Tingluo Huang <tingluohuang@github.com>
    
    * Update src/Test/L0/Worker/ContainerOperationProviderL0.cs
    
    Co-authored-by: Tingluo Huang <tingluohuang@github.com>
    
    * Add back test asserting exception
    
    * Check service exit code if there is no healtcheck configured
    
    * Remove unnecessary healthcheck for healthy service container
    
    * Revert "Check service exit code if there is no healtcheck configured"
    
    This reverts commit fec24e834137afe2dc751a38707d0e399c10f8fb.
    
    Co-authored-by: Ava S <avastancu@github.com>
    Co-authored-by: Tingluo Huang <tingluohuang@github.com>
    
    * Do not fail service containers without the healthcheck
    
    Co-authored-by: JoannaaKL <joannaakl@github.com>
    Co-authored-by: Tingluo Huang <tingluohuang@github.com>

---
+++ src/Test/L0/Worker/ContainerOperationProviderL0.cs
@@ -0,0 +1,126 @@
+using GitHub.Runner.Worker;
+using GitHub.Runner.Worker.Container;
+using Xunit;
+using Moq;
+using GitHub.Runner.Worker.Container.ContainerHooks;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using GitHub.DistributedTask.WebApi;
+using System;
+
+namespace GitHub.Runner.Common.Tests.Worker
+{
+
+    public sealed class ContainerOperationProviderL0
+    {
+
+        private TestHostContext _hc;
+        private Mock<IExecutionContext> _ec;
+        private Mock<IDockerCommandManager> _dockerManager;
+        private Mock<IContainerHookManager> _containerHookManager;
+        private ContainerOperationProvider containerOperationProvider;
+        private Mock<IJobServerQueue> serverQueue;
+        private Mock<IPagingLogger> pagingLogger;
+        private List<string> healthyDockerStatus = new List<string> { "healthy" };
+        private List<string> emptyDockerStatus = new List<string> { string.Empty };
+        private List<string> unhealthyDockerStatus = new List<string> { "unhealthy" };
+        private List<string> dockerLogs = new List<string> { "log1", "log2", "log3" };
+
+        List<ContainerInfo> containers = new List<ContainerInfo>();
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public async void RunServiceContainersHealthcheck_UnhealthyServiceContainer_AssertFailedTask()
+        {
+            //Arrange
+            Setup();
+            _dockerManager.Setup(x => x.DockerInspect(_ec.Object, It.IsAny<string>(), It.IsAny<string>())).Returns(Task.FromResult(unhealthyDockerStatus));
+
+            //Act
+            try
+            {
+                await containerOperationProvider.RunContainersHealthcheck(_ec.Object, containers);
+            }
+            catch (InvalidOperationException)
+            {
+
+                //Assert
+                Assert.Equal(TaskResult.Failed, _ec.Object.Result ?? TaskResult.Failed);
+            }
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public async void RunServiceContainersHealthcheck_UnhealthyServiceContainer_AssertExceptionThrown()
+        {
+            //Arrange
+            Setup();
+            _dockerManager.Setup(x => x.DockerInspect(_ec.Object, It.IsAny<string>(), It.IsAny<string>())).Returns(Task.FromResult(unhealthyDockerStatus));
+
+            //Act and Assert
+            await Assert.ThrowsAsync<InvalidOperationException>(() => containerOperationProvider.RunContainersHealthcheck(_ec.Object, containers));
+
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public async void RunServiceContainersHealthcheck_healthyServiceContainer_AssertSucceededTask()
+        {
+            //Arrange
+            Setup();
+            _dockerManager.Setup(x => x.DockerInspect(_ec.Object, It.IsAny<string>(), It.IsAny<string>())).Returns(Task.FromResult(healthyDockerStatus));
+
+            //Act
+            await containerOperationProvider.RunContainersHealthcheck(_ec.Object, containers);
+
+            //Assert
+            Assert.Equal(TaskResult.Succeeded, _ec.Object.Result ?? TaskResult.Succeeded);
+
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public async void RunServiceContainersHealthcheck_healthyServiceContainerWithoutHealthcheck_AssertSucceededTask()
+        {
+            //Arrange
+            Setup();
+            _dockerManager.Setup(x => x.DockerInspect(_ec.Object, It.IsAny<string>(), It.IsAny<string>())).Returns(Task.FromResult(emptyDockerStatus));
+
+            //Act
+            await containerOperationProvider.RunContainersHealthcheck(_ec.Object, containers);
+
+            //Assert
+            Assert.Equal(TaskResult.Succeeded, _ec.Object.Result ?? TaskResult.Succeeded);
+
+        }
+
+        private void Setup([CallerMemberName] string testName = "")
+        {
+            containers.Add(new ContainerInfo() { ContainerImage = "ubuntu:16.04" });
+            _hc = new TestHostContext(this, testName);
+            _ec = new Mock<IExecutionContext>();
+            serverQueue = new Mock<IJobServerQueue>();
+            pagingLogger = new Mock<IPagingLogger>();
+
+            _dockerManager = new Mock<IDockerCommandManager>();
+            _containerHookManager = new Mock<IContainerHookManager>();
+            containerOperationProvider = new ContainerOperationProvider();
+
+            _hc.SetSingleton<IDockerCommandManager>(_dockerManager.Object);
+            _hc.SetSingleton<IJobServerQueue>(serverQueue.Object);
+            _hc.SetSingleton<IPagingLogger>(pagingLogger.Object);
+
+            _hc.SetSingleton<IDockerCommandManager>(_dockerManager.Object);
+            _hc.SetSingleton<IContainerHookManager>(_containerHookManager.Object);
+
+            _ec.Setup(x => x.Global).Returns(new GlobalContext());
+
+            containerOperationProvider.Initialize(_hc);
+        }
+    }
+}
