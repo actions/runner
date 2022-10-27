a1f68b0b833352f41b113562bee496445fbd412e
Nikola Jokic <nikola-jokic@github.com>
2022-10-27 11:50:02 +0200

    run.sh installs SIGINT and SIGTERM traps to gracefully stop runner

--- src/Misc/layoutroot/run.sh
+++ src/Misc/layoutroot/run.sh
@@ -9,11 +9,22 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
     [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 # run the helper process which keep the listener alive
 while :;
 do
+    # Set job control
+    set -m
+    # On SIGINT or SIGTERM, send SIGINT to Runner.Listener
+    trap "pkill -2 Runner.Listener" INT TERM
+
     cp -f "$DIR"/run-helper.sh.template "$DIR"/run-helper.sh
-    "$DIR"/run-helper.sh $*
+    "$DIR"/run-helper.sh $* &
+    PID=$!
+    wait $PID
+    trap - INT TERM
+    wait $PID
+
     returnCode=$?
     if [[ $returnCode -eq 2 ]]; then
         echo "Restarting runner..."
