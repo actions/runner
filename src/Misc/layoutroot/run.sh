2286c88543767a401b1ac4382f0a3381a2811242
Nikola Jokic <nikola-jokic@github.com>
2022-10-28 15:38:33 +0200

    replaced trap to force wait for child processes and to send kill to run-helper instead of Runner.Listener

--- src/Misc/layoutroot/run.sh
+++ src/Misc/layoutroot/run.sh
@@ -10,26 +10,27 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
+# Set job control
+set -m
+
+trap 'kill -INT -$PID' INT TERM
+
 # run the helper process which keep the listener alive
 while :;
 do
-    # Set job control
-    set -m
-    # On SIGINT or SIGTERM, send SIGINT to Runner.Listener
-    trap "pkill -2 Runner.Listener" INT TERM
-
     cp -f "$DIR"/run-helper.sh.template "$DIR"/run-helper.sh
     "$DIR"/run-helper.sh $* &
     PID=$!
-    wait $PID
-    trap - INT TERM
-    wait $PID
-
+    wait -f $PID
     returnCode=$?
     if [[ $returnCode -eq 2 ]]; then
         echo "Restarting runner..."
     else
         echo "Exiting runner..."
+        # Unregister signal handling before exit
+        trap - INT TERM
+        # wait for last parts to be logged
+        wait $PID
         exit 0
     fi
 done
