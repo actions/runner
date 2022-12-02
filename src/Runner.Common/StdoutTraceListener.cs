d2726670e6eca9c004b852a575071e15530e090b
Ferenc Hammerl <31069338+fhammerl@users.noreply.github.com>
2022-12-02 16:37:27 +0000

    Rewrite TraceListener as superclass

--- src/Runner.Common/StdoutTraceListener.cs
+++ src/Runner.Common/StdoutTraceListener.cs
@@ -5,8 +5,10 @@ using System.Globalization;
 
 namespace GitHub.Runner.Common
 {
-    public sealed class StdoutTraceListener : ConsoleTraceListener
+    public sealed class StdoutTraceListener : RunnerTraceListener
     {
+        public StdoutTraceListener() : base(Console.Out) { }
+
         // Copied and modified slightly from .Net Core source code. Modification was required to make it compile.
         // There must be some TraceFilter extension class that is missing in this source code.
         public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string message)
@@ -19,63 +21,6 @@ namespace GitHub.Runner.Common
             WriteHeader(source, eventType, id);
             WriteLine(message);
             WriteFooter(eventCache);
-        }
-
-        internal bool IsEnabled(TraceOptions opts)
-        {
-            return (opts & TraceOutputOptions) != 0;
-        }
-
-        // Altered from the original .Net Core implementation.
-        private void WriteHeader(string source, TraceEventType eventType, int id)
-        {
-            string type = null;
-            switch (eventType)
-            {
-                case TraceEventType.Critical:
-                    type = "CRIT";
-                    break;
-                case TraceEventType.Error:
-                    type = "ERR ";
-                    break;
-                case TraceEventType.Warning:
-                    type = "WARN";
-                    break;
-                case TraceEventType.Information:
-                    type = "INFO";
-                    break;
-                case TraceEventType.Verbose:
-                    type = "VERB";
-                    break;
-                default:
-                    type = eventType.ToString();
-                    break;
-            }
-
-            Write(StringUtil.Format("[{0:u} {1} {2}] ", DateTime.UtcNow, type, source));
-        }
-
-        // Copied and modified slightly from .Net Core source code to make it compile. The original code
-        // accesses a private indentLevel field. In this code it has been modified to use the getter/setter.
-        private void WriteFooter(TraceEventCache eventCache)
-        {
-            if (eventCache == null)
-                return;
-
-            IndentLevel++;
-            if (IsEnabled(TraceOptions.ProcessId))
-                WriteLine("ProcessId=" + eventCache.ProcessId);
-
-            if (IsEnabled(TraceOptions.ThreadId))
-                WriteLine("ThreadId=" + eventCache.ThreadId);
-
-            if (IsEnabled(TraceOptions.DateTime))
-                WriteLine("DateTime=" + eventCache.DateTime.ToString("o", CultureInfo.InvariantCulture));
-
-            if (IsEnabled(TraceOptions.Timestamp))
-                WriteLine("Timestamp=" + eventCache.Timestamp);
-
-            IndentLevel--;
         }
     }
 }
