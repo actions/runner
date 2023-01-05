b5acea8379b475619f22ad513fd81a4222a98cae
John Sudol <24583161+johnsudol@users.noreply.github.com>
2023-01-05 20:40:38 +0000

    hashFiles using glob and in JS instead

---
+++ src/Misc/expressionFunc/hashFiles/src/hashFiles.js
@@ -0,0 +1,30 @@
+const glob = require('@actions/glob')
+
+async function run() {
+  // arg0 -> node
+  // arg1 -> hashFiles.js
+  // env[followSymbolicLinks] = true/null
+  // env[patterns] -> glob patterns
+  let followSymbolicLinks = false
+  const matchPatterns = process.env.patterns || ''
+  if (process.env.followSymbolicLinks === 'true') {
+    console.log('Follow symbolic links')
+    followSymbolicLinks = true
+  }
+
+  console.log(`Match Pattern: ${matchPatterns}`)
+  try {
+    const result = await glob.hashFiles(
+      matchPatterns,
+      {followSymbolicLinks},
+      true
+    )
+    console.error(`__OUTPUT__${result}__OUTPUT__`)
+    process.exit(0)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+run()
