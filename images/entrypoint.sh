0898c9c6b25043cb9ef1db9d82d337b3ed03212a
Nikola Jokic <jokicnikola07@gmail.com>
2023-03-06 10:26:48 +0100

    update ca if RUNNER_UPDATE_CA env is set

--- images/entrypoint.sh
+++ images/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-# update ca certificates if they are injected with a volume mount
-sudo update-ca-certificates
+# update ca certificates in case they are injected with a volume mount
+function updateCerts() {
+    if [[ ! -z "$(which update-ca-certificates)" ]]; then
+        echo "Running sudo update-ca-certificates"
+        sudo update-ca-certificates
+    elif [[ ! -z "$(which update-ca-trust)" ]]; then
+        echo "Running sudo update-ca-trust"
+        sudo update-ca-trust
+    fi
+}
+
+if [[ ! -z $RUNNER_UPDATE_CA ]]; then
+    updateCerts
+fi
 
 exec ./run.sh $*
