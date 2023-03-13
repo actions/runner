f30a83a4b7f298cb7beff55e45719e7269fd5539
vmjoseph <vmjoseph@github.com>
2023-03-13 10:00:46 -0400

    adding extra catch for download failure in composite actions

--- src/Runner.Worker/ActionManager.cs
+++ src/Runner.Worker/ActionManager.cs
@@ -682,12 +682,14 @@ namespace GitHub.Runner.Worker
                         // * Repo or tag doesn't exist, or isn't public
                         // * Policy validation failed
                         if (ex is WebApi.UnresolvableActionDownloadInfoException)
-                        {
+                        {   // Log the error and fail the JobExtension Initialization.
                             throw;
                         }
                         else
                         {
                             // This exception will be traced as an infrastructure failure
+                            Trace.Error($"Caught exception from JobExtenion Initialization: {ex}");
+                            executionContext.InfrastructureError(ex.Message);
                             throw new WebApi.FailedToResolveActionDownloadInfoException("Failed to resolve action download info.", ex);
                         }
                     }
