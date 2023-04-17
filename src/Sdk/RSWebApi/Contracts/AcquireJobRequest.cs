3a9db5581ca5bc81fc251f30292e3ad13997ef5f
John Wesley Walker III <81404201+jww3@users.noreply.github.com>
2023-04-17 15:50:57 +0200

    Remove Temporary Serialization Shim

--- src/Sdk/RSWebApi/Contracts/AcquireJobRequest.cs
+++ src/Sdk/RSWebApi/Contracts/AcquireJobRequest.cs
@@ -9,27 +9,5 @@ namespace GitHub.Actions.RunService.WebApi
     {
         [DataMember(Name = "jobMessageId", EmitDefaultValue = false)]
         public string JobMessageID { get; set; }
-
-        // The earliest versions of this type serialized the JobMessageID under a field named "streamId"
-        // This private property is expected to be a short-lived shim to aid in transitioning over to the new schema.
-        [DataMember(Name = "streamId", EmitDefaultValue = false)]
-        private string DeprecatedJobMessageIdAlias
-        {
-            get
-            {
-                return this.JobMessageID;
-            }
-            set
-            {
-                // Note that this is a private property, so the only way to reach this setter is via deserialization.
-                // Deserializtion is NOT an expected use case on the Runner side as of early 2023.
-                // In the unlikely event that deserializtion does become a valid use case, we include this guard
-                // to ensure that a valid JobMessageID can't be overwritten by this otherwise unreachable code path.
-                if (string.IsNullOrEmpty(this.JobMessageID))
-                {
-                    this.JobMessageID = value;
-                }
-            }
-        }
     }
 }
