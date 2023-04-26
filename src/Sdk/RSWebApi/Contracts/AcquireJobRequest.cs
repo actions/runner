a41397ae931de1f414a9d34c764aa6df89bebf4a
John Wesley Walker III <81404201+jww3@users.noreply.github.com>
2023-04-26 18:31:41 +0200

    Rename AcquireJobRequest::StreamID to AcquireJobRequest::JobMessageID (#2547)
    * Rename AcquireJobRequest::StreamID to AcquireJobRequest::JobMessageID to match corresponding server-side update.
    * appeased the linter
    * Added unit tests to prove AcquireJobRequest serialization/deserialization is as-expected.
    * Distinguish unit test variations.
    * Incorporated PR Feedback.

--- src/Sdk/RSWebApi/Contracts/AcquireJobRequest.cs
+++ src/Sdk/RSWebApi/Contracts/AcquireJobRequest.cs
@@ -1,13 +1,16 @@
-using System.Collections.Generic;
 using System.Runtime.Serialization;
-using GitHub.DistributedTask.WebApi;
 
 namespace GitHub.Actions.RunService.WebApi
 {
     [DataContract]
     public class AcquireJobRequest
     {
+        [DataMember(Name = "jobMessageId", EmitDefaultValue = false)]
+        public string JobMessageId { get; set; }
+
+        // This field will be removed in an upcoming Runner release.
+        // It's left here temporarily to facilitate the transition to the new field name, JobMessageId.
         [DataMember(Name = "streamId", EmitDefaultValue = false)]
-        public string StreamID { get; set; }
+        public string StreamId { get; set; }
     }
 }
