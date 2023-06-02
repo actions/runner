3a2817a9ae71712d9ea22b72c6983c530c5f7aaa
JoannaaKL <joannaakl@github.com>
2023-06-02 13:30:15 +0200

    Update Constants.cs

--- src/Runner.Common/Constants.cs
+++ src/Runner.Common/Constants.cs
@@ -155,7 +155,7 @@ namespace GitHub.Runner.Common
             {
                 public static readonly string DiskSpaceWarning = "runner.diskspace.warning";
                 public static readonly string Node12Warning = "DistributedTask.AddWarningToNode12Action";
-                public static readonly string LogTemplateErrorsAsDebugMessages = "DistributedTask.LogTemplateErrorAsDebugMessages";
+                public static readonly string LogTemplateErrorsAsDebugMessages = "DistributedTask.LogTemplateErrorsAsDebugMessages";
                 public static readonly string UseContainerPathForTemplate = "DistributedTask.UseContainerPathForTemplate";
                 public static readonly string AllowRunnerContainerHooks = "DistributedTask.AllowRunnerContainerHooks";
             }
