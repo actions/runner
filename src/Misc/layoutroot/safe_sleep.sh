0fe104ded73c3b23ccb2f92b65876aed5e1a5ef4
Fahrzin Hemmati <fahhem@users.noreply.github.com>
2024-02-09 17:42:19 -0800

    Reduce CPU usage of safe_sleep.sh

--- src/Misc/layoutroot/safe_sleep.sh
+++ src/Misc/layoutroot/safe_sleep.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ -x "$(command -v sleep)" ]; then
+  sleep $1
+  exit 0
+fi
+if [ -x "$(command -v ping)" ]; then
+  ping -c $1 127.0.0.1 > /dev/null
+  exit 0
+fi
+
 SECONDS=0
 while [[ $SECONDS != $1 ]]; do
     :
