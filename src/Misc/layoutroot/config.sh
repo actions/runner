620e6ddcdeb4c8cf163c83c833fd3b3802c691b6
Jon Pugh <jon@thinkdrop.net>
2024-03-06 08:28:47 -0500

    Change directory earlier so that the "ldd" commands work when running the script from a different directory.

--- src/Misc/layoutroot/config.sh
+++ src/Misc/layoutroot/config.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Change directory to the script root directory
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd "$DIR"
+
 user_id=`id -u`
 
 # we want to snapshot the environment of the config user
@@ -59,17 +70,6 @@ then
         exit 1
     fi
 fi
-
-# Change directory to the script root directory
-# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-cd "$DIR"
 
 source ./env.sh
 
