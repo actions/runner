104efa51ea98a7d763335a5bb06834e3f862446b
Jacob Wallraff <thyeggman@github.com>
2024-03-18 17:24:33 -0700

    Update ActionManager.cs

--- src/Runner.Worker/ActionManager.cs
+++ src/Runner.Worker/ActionManager.cs
@@ -703,7 +703,7 @@ namespace GitHub.Runner.Worker
                 catch (Exception ex) when (!executionContext.CancellationToken.IsCancellationRequested) // Do not retry if the run is cancelled.
                 {
                     // UnresolvableActionDownloadInfoException is a 422 client error, don't retry
-                    // NonRetryableActionDownloadInfoException is a 400, don't retry, log as infra error
+                    // NonRetryableActionDownloadInfoException is an non-retryable exception from Actions
                     // Some possible cases are:
                     // * Repo is rate limited
                     // * Repo or tag doesn't exist, or isn't public
