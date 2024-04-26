6ab5d540dcc0d38c287dfd2ad831a4f718f99da6
Tingluo Huang <tingluohuang@github.com>
2024-04-26 01:23:04 -0400

    Bump runner to dotnet 8

--- src/Sdk/WebApi/WebApi/OAuth/VssOAuthExceptions.cs
+++ src/Sdk/WebApi/WebApi/OAuth/VssOAuthExceptions.cs
@@ -85,6 +85,7 @@ namespace GitHub.Services.OAuth
             set;
         }
 
+        [Obsolete]
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             base.GetObjectData(info, context);
