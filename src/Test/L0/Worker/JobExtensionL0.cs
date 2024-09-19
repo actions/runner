d549a8bb128c12b8ae57855cd72ef05bb8ed066b
Raj R <58966550+rajrku@users.noreply.github.com>
2024-09-19 01:06:50 +0000

    Lint failure fixes - 3

--- src/Test/L0/Worker/JobExtensionL0.cs
+++ src/Test/L0/Worker/JobExtensionL0.cs
@@ -511,10 +511,7 @@ namespace GitHub.Runner.Common.Tests.Worker
         [Trait("Category", "Worker")]
         public Task EnsureSnapshotPostJobStepForMappingToken_1()
         {
-            var snapshot = new Pipelines.Snapshot("TestImageNameFromMappingToken"){
-                Condition = $"{PipelineTemplateConstants.Success}() && 1==0",
-                Version = "2.*"
-            };
+            var snapshot = new Pipelines.Snapshot("TestImageNameFromMappingToken", condition: $"{PipelineTemplateConstants.Success}() && 1==0", version: "2.*");
             var imageNameValueStringToken = new StringToken(null, null, null, snapshot.ImageName);
             var condition = new StringToken(null, null, null, snapshot.Condition);
             var version = new StringToken(null, null, null, snapshot.Version);
