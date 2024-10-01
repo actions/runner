2ae0914c44ac90b13b931c637a618afbf02bcb2d
Raj R <58966550+rajrku@users.noreply.github.com>
2024-10-01 00:14:49 +0000

    Lint fixes

--- src/Sdk/DTPipelines/Pipelines/ObjectTemplating/PipelineTemplateConverter.cs
+++ src/Sdk/DTPipelines/Pipelines/ObjectTemplating/PipelineTemplateConverter.cs
@@ -400,9 +400,9 @@ namespace GitHub.DistributedTask.Pipelines.ObjectTemplating
         {
             var versionSegments = versionString.Split(".");
 
-            if (versionSegments.Length != 2 || 
-                !versionSegments[1].Equals("*") || 
-                !Int32.TryParse(versionSegments[0], NumberStyles.None, CultureInfo.InvariantCulture, result: out int parsedMajor) || 
+            if (versionSegments.Length != 2 ||
+                !versionSegments[1].Equals("*") ||
+                !Int32.TryParse(versionSegments[0], NumberStyles.None, CultureInfo.InvariantCulture, result: out int parsedMajor) ||
                 parsedMajor < 0)
             {
                 return false;
