9c36e5d1bab4edfeebc4889f970c48fa44a205e6
ejking <eric@kinghometech.com>
2024-10-08 21:13:29 -0400

    Update FeatureManager.cs

--- src/Runner.Worker/FeatureManager.cs
+++ src/Runner.Worker/FeatureManager.cs
@@ -11,5 +11,25 @@ namespace GitHub.Runner.Worker
             var isContainerHooksPathSet = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(Constants.Hooks.ContainerHooksPath));
             return isContainerHookFeatureFlagSet && isContainerHooksPathSet;
         }
+    
+        public static void EnsureContainerOperationsFeature(Variables variables)
+        {
+            if (Constants.Runner.Platform.Equals(Constants.OSPlatform.Linux))
+            {
+                return;
+            }
+            else if(Constants.Runner.Platform.Equals(Constants.OSPlatform.Windows))
+            {
+                var isContainerOnWindowsFeatureFlagSet = bool.TryParse(Environment.GetEnvironmentVariable(Constants.Runner.Features.AllowContainerOperationsOnWindows) ?? "false", out var b) && b;
+                if (!isContainerOnWindowsFeatureFlagSet)
+                {
+                    throw new NotSupportedException($"Container operations are not supported on Windows runners (experimental support can be enabled via '{Constants.Runner.Features.AllowContainerOperationsOnWindows}')");
+                }
+            }
+            else
+            {
+                throw new NotSupportedException($"Container operations are not supported on macOS runners");
+            }
+        }
     }
 }
