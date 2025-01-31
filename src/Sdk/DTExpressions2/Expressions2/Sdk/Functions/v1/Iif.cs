7a652f2e304ca35a0d3d5b0719e83adaa5d559c0
ChristopherHX <christopher.homberger@web.de>
2025-01-31 16:22:54 +0100

    azp: iif allow complex objects / disable short circuit (#535)

--- src/Sdk/DTExpressions2/Expressions2/Sdk/Functions/v1/Iif.cs
+++ src/Sdk/DTExpressions2/Expressions2/Sdk/Functions/v1/Iif.cs
@@ -8,11 +8,11 @@ namespace GitHub.DistributedTask.Expressions2.Sdk.Functions.v1
 
         protected sealed override Object EvaluateCore(EvaluationContext context, out ResultMemory memory)
         {
-            // TODO learn the semantics of this function
             memory = null;
             bool cond = Parameters[0].EvaluateBoolean(context);
-            String body = Parameters[cond ? 1 : 2].EvaluateString(context) as String ?? String.Empty;
-            return body;
+            EvaluationResult then = Parameters[1].Evaluate(context);
+            EvaluationResult otherwise = Parameters[2].Evaluate(context);
+            return cond ? then.Raw ?? then.Value : otherwise.Raw ?? otherwise.Value;
         }
     }
 }
