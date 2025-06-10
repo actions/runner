a27d682de45e1b2bdadbbdc01fff8738332d8e54
fenghao <38210394+deanjingshui@users.noreply.github.com>
2025-06-10 12:00:01 +0800

    chore: delete unused windows dockerfiles
    the unused windows dockerfiles once used to run raw machine,
    now we use kubernetes replace it.

--- images/bootstrap.ps1
+++
@@ -1,6 +0,0 @@
-cd $env:RUNNER_HOME
-if (!(Test-Path .\_work)) {
-  .\config.cmd --unattended --url https://github.com/web-infra-dev --name "$env:RUNNER_NAME" --labels "$env:RUNNER_LABELS" --token "$env:RUNNER_TOKEN" --runnergroup default --work _work --disableupdate;
-}
-
-.\run.cmd
