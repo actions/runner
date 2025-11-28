6910cfe097cc4e2ce78acb3d8ab03288373325fb
Kosa Matyas <kosa03matyas@gmail.com>
2025-11-28 18:43:02 +0200

    safe_sleep: now is safe and it does sleep

--- src/Misc/layoutroot/safe_sleep.sh
+++ src/Misc/layoutroot/safe_sleep.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-SECONDS=0
-while [[ $SECONDS -lt $1 ]]; do
-    :
-done
+if command -v sleep > /dev/null 2>&1; then
+    sleep "$1"
+else
+    SECONDS=0
+    while [[ $SECONDS -lt $1 ]]; do
+        :
+    done
+fi
