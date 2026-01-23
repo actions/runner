ecf0904cb6a85dc64bc728c02bf67dcb06dfe4c9
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-01-22 22:42:48 -0500

    spelling: variables
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/Runner.Common/JobServerQueue.cs
+++ src/Runner.Common/JobServerQueue.cs
@@ -183,7 +183,7 @@ namespace GitHub.Runner.Common
         }
 
         // WebConsoleLine queue and FileUpload queue are always best effort
-        // TimelineUpdate queue error will become critical when timeline records contain output variabls.
+        // TimelineUpdate queue error will become critical when timeline records contain output variables.
         public async Task ShutdownAsync()
         {
             if (!_queueInProcess)
@@ -214,7 +214,7 @@ namespace GitHub.Runner.Common
             Trace.Info("Results upload queue drained.");
 
             // ProcessTimelinesUpdateQueueAsync() will throw exception during shutdown
-            // if there is any timeline records that failed to update contains output variabls.
+            // if there is any timeline records that failed to update contains output variables.
             Trace.Verbose("Draining timeline update queue.");
             await ProcessTimelinesUpdateQueueAsync(runOnce: true);
             Trace.Info("Timeline update queue drained.");
