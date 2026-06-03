088775a50e81b084c520dbd3812f8786c32772ee
Lokesh Gopu <lokesh755@github.com>
2026-06-03 14:39:05 -0700

    Add SDK types and results plumbing for background step control

--- src/Sdk/DTPipelines/Pipelines/StepConverter.cs
+++ src/Sdk/DTPipelines/Pipelines/StepConverter.cs
@@ -51,6 +51,9 @@ namespace GitHub.DistributedTask.Pipelines
                     case StepType.Action:
                         stepObject = new ActionStep();
                         break;
+                    case StepType.BackgroundStepControl:
+                        stepObject = new BackgroundStepControl();
+                        break;
                 }
 
                 using (var objectReader = value.CreateReader())
